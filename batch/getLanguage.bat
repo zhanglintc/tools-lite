a68a96d10153dd3aba27572ea891f7519934f5f2
Lane <zhanglintc623@gmail.com>
2014-05-08 21:00:56 +0800

    new language judge

--- batch/getLanguage.bat
+++ batch/getLanguage.bat
@@ -2,7 +2,11 @@
 for /f "tokens=3" %%i in ('reg query "HKCU\Control Panel\International" /v "sLanguage"') do (
     set reg_localevar=%%i
 )
+for /f "tokens=3" %%i in ('reg query "HKCU\Control Panel\Desktop" /v "PreferredUILanguages"') do (
+    set reg_uilanguage=%%i
+)
 
 echo %reg_localevar%
+echo %reg_uilanguage%
 
 pause>nul
