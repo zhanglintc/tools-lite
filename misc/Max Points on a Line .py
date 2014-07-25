fccdfea9d3aa9cf503bd34db3c52b76e902cd796
Lane <zhanglintc623@gmail.com>
2014-07-25 01:06:16 -0700

    fix

--- misc/Max Points on a Line .py
+++ misc/Max Points on a Line .py
@@ -9,7 +9,7 @@ class Solution:
     # @return an integer
     def maxPoints(self, points):
         max_counter = 0
-        cur_coutner = 2
+        cur_coutner = 1
         k_init = None
         k_curr = None
 
@@ -25,25 +25,40 @@ class Solution:
                     continue
                 else: # not same point
                     if k_init == None: # get first k
-                        if points[i].x - points[j].x == 0: # horizontal
+                        if points[i].x - points[j].x == 0: # vertical
                             k_init = -1
                         else: # normal
-                            k_init = (points[i].y - points[j].y) / (points[i].x - points[j].x)
+                            k_init = float((points[i].y - points[j].y) / (points[i].x - points[j].x))
+                        cur_coutner += 1
+                        continue
                     else: # current k
-                        if points[i].x - points[j].x == 0: # horizontal
+                        if points[i].x - points[j].x == 0: # vertical
                             k_curr = -1
                         else: # normal
-                            k_curr = (points[i].y - points[j].y) / (points[i].x - points[j].x)
+                            k_curr = float((points[i].y - points[j].y) / (points[i].x - points[j].x))
 
                 if k_curr == k_init:
                     cur_coutner += 1
 
             if cur_coutner > max_counter:
                 max_counter = cur_coutner
-            cur_coutner = 2
+            cur_coutner = 1
             k_init = None
+            k_curr = None
 
         return max_counter
 
+class Point:
+    def __init__(self, a=0, b=0):
+        self.x = a
+        self.y = b
+
+points = []
+lst = [(-4,-4),(-8,-582),(-3,3),(-9,-651),(9,591)]
+for i in lst:
+    point = Point()
+    point.x, point.y = i
+    points.append(point)
+
 S = Solution()
-S.maxPoints([(0,0),(1,1),(1,-1)])
+print(S.maxPoints(points))
