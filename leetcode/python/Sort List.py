113be27d544e4826d69358d61d50d0aa65059c35
Lane <zhanglintc623@gmail.com>
2014-08-14 00:23:56 +0800

    Quick Sort revision

--- leetcode/python/Sort List.py
+++ leetcode/python/Sort List.py
@@ -7,50 +7,82 @@
 class Solution:
     # @param head, a ListNode
     # @return a ListNode
-    def merge(self, lst1, lst2):
-        if lst1 == None:
-            return lst2
-        if lst2 == None:
-            return lst1
-
-        new_lst = ListNode(0) # new a linded list
-        pointer = new_lst
-
-        while lst1 != None and lst2 != None:
-            if lst1.val < lst2.val:
-                pointer.next = lst1
-                lst1 = lst1.next
-            else:
-                pointer.next = lst2
-                lst2 = lst2.next
-            pointer = pointer.next
-
-        if lst1 == None:
-            pointer.next = lst2
-        if lst2 == None:
-            pointer.next = lst1
-
-        return new_lst.next
-
-        
     def sortList(self, head):
         if head == None or head.next == None: # only one node
             return head
 
-        fast = head
-        slow = head
+        new_head_1st = ListNode(0) # two new head
+        new_head_2nd = ListNode(0)
+        pointer = head.next # skip the first node
 
-        while fast.next != None and fast.next.next != None:
-            fast = fast.next.next
-            slow = slow.next
+        cur_1st = new_head_1st
+        cur_2nd = new_head_2nd
 
-        new_head_1st = head
-        new_head_2nd = slow.next
-        slow.next = None #cut the linked list
+        while pointer != None: # until the end
+            if pointer.val <= head.val:
+                cur_1st.next = pointer
+                cur_1st = cur_1st.next
+            else:
+                cur_2nd.next = pointer
+                cur_2nd = cur_2nd.next
+            pointer = pointer.next # point to the next one
 
-        new_head_1st = self.sortList(new_head_1st)
-        new_head_2nd = self.sortList(new_head_2nd)
+        cur_1st.next = None # cut the end
 
-        return self.merge(new_head_1st, new_head_2nd)
+        cur_2nd.next = head # add the first node to the end of cur_2nd
+        head.next = None # cut the end
 
-# This version is using Merge Sort, but failed bacause of LTE.
+        cur_1st = new_head_1st.next
+        cur_2nd = new_head_2nd.next
+
+        cur_1st = self.sortList(cur_1st)
+        cur_2nd = self.sortList(cur_2nd)
+
+        pointer = cur_1st
+
+        if pointer == None:
+            return cur_2nd
+
+        while pointer.next != None: # find the end
+            pointer = pointer.next
+
+        pointer.next = cur_2nd
+
+        return cur_1st
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+lst = [10,223,88,1,3,11]
+
+head = ListNode(0)
+pt = head # point to the head
+for i in lst:
+    node = ListNode(i) # new a node
+    if head.next == None:
+        head.next = node
+    else:
+        pt.next = node
+    pt = pt.next
+
+# pt = head.next
+# while pt != None:
+#     print (pt.val)
+#     pt = pt.next
+
+def debug_print(lst):
+    while lst != None:
+        print (lst.val)
+        lst = lst.next
+
+pt = head.next
+
+S = Solution()
+
+pt = S.sortList(pt)
+
+while pt != None:
+    print (pt.val)
+    pt = pt.next
