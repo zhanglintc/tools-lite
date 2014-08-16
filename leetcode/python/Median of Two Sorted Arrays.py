bdc5be6a1e3035e3bd804d958ccedb47c66cb8e6
Lane <zhanglintc623@gmail.com>
2014-08-16 23:53:57 +0800

    AC edition of "Median of Two Sorted Arrays"

---
+++ leetcode/python/Median of Two Sorted Arrays.py
@@ -0,0 +1,29 @@
+class Solution:
+    # @return a float
+    def findMedianSortedArrays(self, A, B):
+        new_list = []
+
+        while A != [] and B != []:
+            if A[0] < B[0]:
+                new_list.append(A.pop(0))
+            else:
+                new_list.append(B.pop(0))
+
+        if A == []:
+            new_list += B
+        if B == []:
+            new_list += A
+
+        if len(new_list) % 2 == 1:
+            return new_list[(len(new_list) - 1) / 2]
+        else:
+            left  = new_list[(len(new_list) - 1) / 2]
+            right = new_list[len(new_list) / 2]
+            return (float(left) + float(right)) / 2
+
+A = [1]
+B = [1]
+
+S = Solution()
+
+S.findMedianSortedArrays(A, B)
