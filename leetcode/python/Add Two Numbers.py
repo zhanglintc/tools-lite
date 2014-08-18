f425b194d0b159aa2f2621295dbd31b090a0bf76
Lane <zhanglintc623@gmail.com>
2014-08-18 22:28:59 +0800

    AC

--- leetcode/python/Add Two Numbers.py
+++ leetcode/python/Add Two Numbers.py
@@ -11,32 +11,58 @@ class Solution:
             return None
 
         new_head = ListNode(0)
+        cache = new_head
         cur = new_head
         add_flag = 0
 
+        # both list
         while l1 != None and l2 != None:
             cur.val = ((l1.val + l2.val + 1) if add_flag else (l1.val + l2.val))
+            add_flag = 0
 
             if cur.val >= 10:
                 cur.val %= 10
                 add_flag = 1
-            
+
+            cur.next = ListNode(0)
+            cache = cur
+            cur = cur.next
             l1 = l1.next
             l2 = l2.next
 
-            if l1 == None and l2 != None:
-                cur.next = l2
-                break
-            elif l2 == None and l1 != None:
-                cur.next = l1
-                break
-            elif l1 == None and l2 == None and add_flag == 0:
-                cur.next = None
-                break
+        # l1 and !l2
+        while l1 != None and l2 == None:
+            cur.val = ((l1.val + 1) if add_flag else l1.val)
+            add_flag = 0
+
+            if cur.val >= 10:
+                cur.val %= 10
+                add_flag = 1
 
             cur.next = ListNode(0)
+            cache = cur
             cur = cur.next
-            add_flag = 0  
+            l1 = l1.next
+
+        # !l1 and l2
+        while l1 == None and l2 != None:
+            cur.val = ((l2.val + 1) if add_flag else l2.val)
+            add_flag = 0
+
+            if cur.val >= 10:
+                cur.val %= 10
+                add_flag = 1
+
+            cur.next = ListNode(0)
+            cache = cur
+            cur = cur.next
+            l2 = l2.next
+
+        # set the tail
+        if add_flag == 1:
+            cache.next = ListNode(1)
+        else:
+            cache.next = None
 
         return new_head
 
