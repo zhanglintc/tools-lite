8340743c56fcfa4ab2a0cfad87283f31908c5856
Lane <zhanglintc623@gmail.com>
2014-08-21 00:00:37 +0800

    init

---
+++ leetcode/python/Binary Tree Maximum Path Sum.py
@@ -0,0 +1,16 @@
+# Binary Tree Maximum Path Sum
+# for leetcode problems
+# 2014.08.20 by zhanglin
+
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return an integer
+    def maxPathSum(self, root):
+        pass
