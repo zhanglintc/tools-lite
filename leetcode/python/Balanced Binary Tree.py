76a368cf64751b6845ee40d567e46c2ce4be125f
Lane <zhanglintc623@gmail.com>
2014-08-21 13:18:09 +0800

    init

---
+++ leetcode/python/Balanced Binary Tree.py
@@ -0,0 +1,16 @@
+# Balanced Binary Tree
+# for leetcode problems
+# 2014.08.21 by zhanglin
+
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a boolean
+    def isBalanced(self, root):
+        
