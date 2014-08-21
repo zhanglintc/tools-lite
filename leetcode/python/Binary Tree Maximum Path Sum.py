993e0f129757f3b4d76264553ff0358713fc9077
Lane <zhanglintc623@gmail.com>
2014-08-21 12:06:42 +0800

    AC

--- leetcode/python/Binary Tree Maximum Path Sum.py
+++ leetcode/python/Binary Tree Maximum Path Sum.py
@@ -12,5 +12,21 @@
 class Solution:
     # @param root, a tree node
     # @return an integer
+    def maxPathSum_helper(self, root, lst):
+        if root == None:
+            return 0
+
+        left  = self.maxPathSum_helper(root.left,  lst) # find max of left
+        right = self.maxPathSum_helper(root.right, lst) # find max of right
+        max_sum_add = max(root.val, left + root.val, right + root.val, left + root.val + right) # for append to lst
+        max_sum_ret = max(root.val, left + root.val, right + root.val) # for return
+        lst.append(max_sum_add) # add this node's max to lst
+        return max_sum_ret # return max value through this node
+
     def maxPathSum(self, root):
-        pass
+        lst = []
+        self.maxPathSum_helper(root, lst)
+        if lst != []:
+            return max(lst)
+        # other: (means lst == [])
+        #   return None
