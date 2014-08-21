67e29bf1c205e437232b0dc9b937e7805425eb93
Lane <zhanglintc623@gmail.com>
2014-08-21 17:44:31 +0800

    AC

---
+++ leetcode/python/Two Sum.py
@@ -0,0 +1,13 @@
+# Two Sum
+# for leetcode problems
+# 2014.07.4 by zhanglin
+
+class Solution:
+    # @return a tuple, (index1, index2)
+    def twoSum(self, num, target):
+        d = {}
+        for i in range(len(num)):
+            if num[i] in d:
+                return (d[num[i]] + 1, i + 1)
+            else:
+                d[target - num[i]] = i
