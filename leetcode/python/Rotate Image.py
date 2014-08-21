343ea02bf0885e2913dbc0f4c9b2e0f4edda68a3
Lane <zhanglintc623@gmail.com>
2014-08-22 00:40:18 +0800

    middle
    公式不对

--- leetcode/python/Rotate Image.py
+++ leetcode/python/Rotate Image.py
@@ -6,4 +6,13 @@ class Solution:
     # @param matrix, a list of lists of integers
     # @return a list of lists of integers
     def rotate(self, matrix):
-        pass
+        length = len(matrix)
+        if length <= 1:
+            return matrix
+
+        new_matrix = matrix
+        for i in range(length):
+            for j in range(length):
+                new_matrix[j][length - 1 - i] = matrix[i][j]
+
+        return new_matrix
