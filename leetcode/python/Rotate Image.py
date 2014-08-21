cde0197d76dbd5c6308421916bfde0c1d06a5acd
Lane <zhanglintc623@gmail.com>
2014-08-21 18:19:17 +0800

    init

---
+++ leetcode/python/Rotate Image.py
@@ -0,0 +1,9 @@
+# Rotate Image
+# for leetcode problems
+# 2014.08.21 by zhanglin
+
+class Solution:
+    # @param matrix, a list of lists of integers
+    # @return a list of lists of integers
+    def rotate(self, matrix):
+        pass
