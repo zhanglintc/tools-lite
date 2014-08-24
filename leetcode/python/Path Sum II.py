63a75161e653dbca6184a2cebd0fd40ccd9c33d8
Lane <zhanglintc623@gmail.com>
2014-08-24 23:29:27 +0800

    init

---
+++ leetcode/python/Path Sum II.py
@@ -0,0 +1,18 @@
+# Path Sum II
+# for leetcode problems
+# 2014.08.24 by zhanglin
+
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @param sum, an integer
+    # @return a list of lists of integers
+    def pathSum(self, root, sum):
+        pass
+
