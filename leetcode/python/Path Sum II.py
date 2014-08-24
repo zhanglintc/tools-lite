70015fb4c630956ea94ff3dc9589f6b51eece69b
Lane <zhanglintc623@gmail.com>
2014-08-25 01:04:04 +0800

    AC
    小意思

--- leetcode/python/Path Sum II.py
+++ leetcode/python/Path Sum II.py
@@ -13,6 +13,26 @@ class Solution:
     # @param root, a tree node
     # @param sum, an integer
     # @return a list of lists of integers
+    def pahtSum_helper(self, root, sum, thisSum, thisList, mainList):
+        thisList.append(root.val)
+
+        if root.left == None and root.right == None:
+            if sum == root.val + thisSum:
+                mainList.append(thisList[:])
+
+        if root.left != None:
+            self.pahtSum_helper(root.left, sum, thisSum + root.val, thisList, mainList)
+            thisList.pop()
+
+        if root.right != None:
+            self.pahtSum_helper(root.right, sum, thisSum + root.val, thisList, mainList)
+            thisList.pop()
+
     def pathSum(self, root, sum):
-        pass
+        thisList = []
+        mainList = []
+        if root != None:
+            self.pahtSum_helper(root, sum, 0, thisList, mainList)
+
+        return mainList
 
