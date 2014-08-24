f93730f156d5e6a59a07f13ab629b2639a11b8c6
Lane <zhanglintc623@gmail.com>
2014-08-25 01:07:36 +0800

    init

---
+++ leetcode/python/Same Tree.py
@@ -0,0 +1,18 @@
+# Same Tree
+# for leetcode problems
+# 2014.08.25 by zhanglin
+
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param p, a tree node
+    # @param q, a tree node
+    # @return a boolean
+    def isSameTree(self, p, q):
+        pass
+
