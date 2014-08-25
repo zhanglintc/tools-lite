c508cc055adb684552a37ff3cabb9ba58a23b9ba
Lane <zhanglintc623@gmail.com>
2014-08-25 13:40:48 +0800

    INIT

---
+++ leetcode/python/Flatten Binary Tree to Linked List.py
@@ -0,0 +1,16 @@
+# Flatten Binary Tree to Linked List
+# for leetcode problems
+# 2014.08.25 by zhanglin
+
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return nothing, do it in place
+    def flatten(self, root):
+        
