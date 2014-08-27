5dced4e216b6f46dc088ec14a8b4b6d55ea5c13b
Lane <zhanglintc623@gmail.com>
2014-08-28 01:41:44 +0800

    INIT

---
+++ leetcode/c++/Convert Sorted Array to Binary Search Tree.cpp
@@ -0,0 +1,30 @@
+# Convert Sorted Array to Binary Search Tree
+# for leetcode problems
+# 2014.08.28 by zhanglin
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    TreeNode *sortedListToBST(ListNode *head)
+    {
+        ;
+    }
+};
+
