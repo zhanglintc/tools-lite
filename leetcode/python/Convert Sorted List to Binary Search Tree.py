e61cc41f55d0c5397a3b4b5b214ca9e66cf29632
Lane <zhanglintc623@gmail.com>
2014-08-27 19:45:56 +0800

    MIDDLE

--- leetcode/python/Convert Sorted List to Binary Search Tree.py
+++ leetcode/python/Convert Sorted List to Binary Search Tree.py
@@ -3,17 +3,17 @@
 # 2014.08.27 by zhanglin
 
 # Definition for a  binary tree node
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 #
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
     # @param head, a list node
@@ -28,17 +28,26 @@ class Solution:
 
         fast = head
         slow = head
+        prev = head
         root.left  = TreeNode(0)
         root.right = TreeNode(0)
 
         while fast.next != None and fast.next.next != None:
             fast = fast.next.next
+            prev = slow
             slow = slow.next
         
-        if fast.next == None:
-            pass
-        elif fast.next.next == None:
-            pass
+        if fast.next == None: # odd
+            left = head
+            right = slow.next
+            root.val = slow.val
+            prev.next = None
+
+        elif fast.next.next == None: # even
+            left = head
+            right = slow.next.next
+            root.val = slow.next.val
+            prev.next = None
 
         root.left  = self.sortedListToBST_helper(left,  root.left)
         root.right = self.sortedListToBST_helper(right, root.right)
@@ -53,3 +62,9 @@ Input:  {1,3}
 Output: {1,1,3}
 Expected:   {3,1}
 '''
+
+head = ListNode(1)
+head.next = ListNode(2)
+
+s = Solution()
+s.sortedListToBST(head)
