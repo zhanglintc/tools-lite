02da77dbf87810f016615fa9148e75ea724efdfe
Lane <zhanglintc623@gmail.com>
2014-08-28 16:51:38 +0800

    MIDDLE

--- leetcode/c++/Convert Sorted Array to Binary Search Tree.cpp
+++ leetcode/c++/Convert Sorted Array to Binary Search Tree.cpp
@@ -1,6 +1,6 @@
-# Convert Sorted Array to Binary Search Tree
-# for leetcode problems
-# 2014.08.28 by zhanglin
+// Convert Sorted Array to Binary Search Tree
+// for leetcode problems
+// 2014.08.28 by zhanglin
 
 /**
  * Definition for singly-linked list.
@@ -22,9 +22,77 @@
 class Solution
 {
 public:
+    TreeNode *sortedListToBST_helper(ListNode *head, TreeNode *root)
+    {
+        root->left  = new TreeNode(0);
+        root->right = new TreeNode(0);
+        ListNode *fast = head;
+        ListNode *slow = head;
+        ListNode *prev = head;
+
+        ListNode *left  = NULL; // here
+        ListNode *right = NULL; // here
+
+        if(head == NULL) // no node
+        {
+            return NULL;
+        }
+
+        if(head->next == NULL) // one node
+        {
+            root->val = head->val;
+            root->left  = NULL;
+            root->right = NULL;
+            return root;
+        }
+
+        if(head->next->next == NULL) // two nodes
+        {
+            root->val   = head->next->val;
+            head->next  = NULL;
+            root->left  = sortedListToBST_helper(head,  root->left);
+            root->right = sortedListToBST_helper(NULL, root->right);
+            return root;
+        }
+
+        while(fast->next != NULL && fast->next->next != NULL) // three or more nodes
+        {
+            fast = fast->next->next;
+            prev = slow;
+            slow = slow->next;
+        }
+
+        if(fast->next == NULL) // odd
+        {
+            left = head;
+            right = slow->next;
+            root->val = slow->val;
+            prev->next = NULL;
+        }
+
+        else if(fast->next->next == NULL) // even
+        {
+            left = head;
+            right = slow->next->next;
+            root->val = slow->next->val;
+            prev->next = NULL;
+        }
+
+        root->left  = sortedListToBST_helper(left,  root->left);
+        root->right = sortedListToBST_helper(right, root->right);
+
+        return root;
+    }
+
     TreeNode *sortedListToBST(ListNode *head)
     {
-        ;
+        TreeNode *root = new TreeNode(0);
+        return sortedListToBST_helper(head, root);
     }
 };
 
+/*
+            {-1,0,1,2}
+Output:     {1,-1,2}
+Expected:   {1,0,2,-1}
+*/
