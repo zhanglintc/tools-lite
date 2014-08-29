dca14636926c7feded9c3f0d0bdb379d62b82834
Lane <zhanglintc623@gmail.com>
2014-08-29 18:06:08 +0800

    INIT

---
+++ leetcode/python/Unique Binary Search Trees.py
@@ -0,0 +1,15 @@
+# Unique Binary Search Trees
+# for leetcode problems
+# 2014.08.29 by zhanglin
+
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @return an integer
+    def numTrees(self, n):
+        pass
