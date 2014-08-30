851f80166034d757064f6d02ee105e8547a01613
Lane <zhanglintc623@gmail.com>
2014-08-31 01:57:09 +0800

    INIT

---
+++ leetcode/python/Unique Binary Search Trees II.py
@@ -0,0 +1,27 @@
+# Unique Binary Search Trees II
+# for leetcode problems
+# 2014.08.31 by zhanglin
+
+# Problem:
+# Given n, generate all structurally unique BST's (binary search trees) that store values 1...n.
+
+# For example,
+# Given n = 3, your program should return all 5 unique BST's shown below.
+
+#    1         3     3      2      1
+#     \       /     /      / \      \
+#      3     2     1      1   3      2
+#     /     /       \                 \
+#    2     1         2                 3
+
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @return a list of tree node
+    def generateTrees(self, n):
+        pass
