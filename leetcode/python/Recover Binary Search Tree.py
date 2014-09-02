3054eae0296a3114e26fccced3d941ca05dfa050
Lane <zhanglintc623@gmail.com>
2014-09-02 17:52:25 +0800

    AC 哎哟我擦，累死爹了

--- leetcode/python/Recover Binary Search Tree.py
+++ leetcode/python/Recover Binary Search Tree.py
@@ -20,6 +20,29 @@
 class Solution:
     # @param root, a tree node
     # @return a tree node
-    def recoverTree(self, root):
-        pass
+    def recoverTree_helper(self, root, lst): # lst[0]: pre,  lst[1]: mistake1,  lst[2]: mistake2
+        if root == None:
+            return root
 
+        self.recoverTree_helper(root.left, lst)
+
+        if lst[0] == None:
+            lst[0] = root
+
+        else:
+            if lst[0].val > root.val and lst[1] == None:
+                lst[1] = lst[0]
+                lst[2] = root
+
+            elif lst[0].val > root.val and lst[1] != None:
+                lst[2] = root
+
+            lst[0] = root
+
+        self.recoverTree_helper(root.right, lst)
+
+    def recoverTree(self, root):
+        lst = [None, None, None]
+        self.recoverTree_helper(root, lst)
+        lst[1].val, lst[2].val = lst[2].val, lst[1].val
+        return root
