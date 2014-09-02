96bd5d474f08a80ace4d535d3cda92e89fcb33c1
Lane <zhanglintc623@gmail.com>
2014-09-02 23:07:32 +0800

    AC
    括号对应的修改

--- leetcode/python/Same Tree.py
+++ leetcode/python/Same Tree.py
@@ -17,7 +17,7 @@ class Solution:
         if p == None and q == None:
             return True
 
-        if ((p == None and q != None) or (p != None and q == None) or p.val != q.val):
+        if (p == None and q != None) or (p != None and q == None) or (p.val != q.val):
             return False
 
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
