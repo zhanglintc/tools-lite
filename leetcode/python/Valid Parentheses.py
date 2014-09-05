f5005c6cc7bdeac5add36892747b8ae38516ba79
Lane <zhanglintc623@gmail.com>
2014-09-05 13:37:55 +0800

    INIT

---
+++ leetcode/python/Valid Parentheses.py
@@ -0,0 +1,14 @@
+# Valid Parentheses
+# for leetcode problems
+# 2014.09.05 by zhanglin
+
+# Problem:
+# Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+
+class Solution:
+    # @return a boolean
+    def isValid(self, s):
+        pass
+
