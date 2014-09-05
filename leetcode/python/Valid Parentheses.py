dc8b7a3e6390b968440e3438bb00dbbc4e06a4d7
Lane <zhanglintc623@gmail.com>
2014-09-05 17:10:49 +0800

    AC

--- leetcode/python/Valid Parentheses.py
+++ leetcode/python/Valid Parentheses.py
@@ -10,5 +10,28 @@
 class Solution:
     # @return a boolean
     def isValid(self, s):
-        pass
+        stack = []
+        left  = "({["
+        right = ")}]"
+
+        for item in s:
+            if item in right and stack == []: # ")}]" occur but stack is None
+                return False
+
+            if item in left: # if "({[", push item to stack
+                stack.append(item)
+
+            elif item in right: # if ")}]"
+                if stack[-1] == left[right.index(item)]: # if match last item in stack
+                    stack.pop() # pop one item in stack and continue
+                else: # else return false
+                    return False
+
+        # after traversal
+        if stack == []:
+            return True
+
+        else:
+            return False
+
 
