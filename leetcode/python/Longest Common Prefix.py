fc61d7396ce869d54096010ba2c4291ccdd9176e
Lane <zhanglintc623@gmail.com>
2014-09-08 21:40:37 +0800

    AC

--- leetcode/python/Longest Common Prefix.py
+++ leetcode/python/Longest Common Prefix.py
@@ -9,6 +9,30 @@
 class Solution:
     # @return a string
     def longestCommonPrefix(self, strs):
-        
+        ret_str = ''
+
+        if strs == []:
+            return ret_str
+
+        short = strs[0] # find the shortest string to save time
+        for s in strs:
+            if s < short:
+                short = s
+
+        for i in range(len(short)): # traverse the shortest string
+            add = True
+            for s in strs: # traverse the whole strs
+                if short[i] == s[i]: # find this char, jump over
+                    continue
+                else: # doesn't find this char, set the flag as false, and break
+                    add = False
+                    break
+
+            if add == False: # if the flag is false, break
+                break
+
+            ret_str += s[i] # if everthing is OK, add this char to the end of ret_str
+
+        return ret_str
 
 
