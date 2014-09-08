369bc6a41060a415593a86e8ee7db89eff499db6
Lane <zhanglintc623@gmail.com>
2014-09-08 21:00:57 +0800

    INIT

---
+++ leetcode/python/Longest Common Prefix.py
@@ -0,0 +1,14 @@
+# Longest Common Prefix
+# for leetcode problems
+# 2014.09.08 by zhanglin
+
+# Problem:
+# Write a function to find the longest common prefix string amongst an array of strings.
+# Don's know what longest common prefix is? See http://en.wikipedia.org/wiki/LCP_array
+
+class Solution:
+    # @return a string
+    def longestCommonPrefix(self, strs):
+        
+
+
