3e9e0e78fa6bdef11fa6d0fb78c29aa59899248c
Lane <zhanglintc623@gmail.com>
2014-09-08 23:49:28 +0800

    INIT

---
+++ leetcode/python/Longest Palindromic Substring.py
@@ -0,0 +1,18 @@
+# Longest Palindromic Substring
+# for leetcode problems
+# 2014.09.08 by zhanglin
+
+# Problem:
+# Given a string S, find the longest palindromic substring in S.
+# You may assume that the maximum length of S is 1000,
+# and there exists one unique longest palindromic substring.
+
+# What is Palindrome:
+# Palindrome is a word, phrase, or sequence that reads the same backwards as forwards,
+# e.g. madam or nurses run.
+
+class Solution:
+    # @return a string
+    def longestPalindrome(self, s):
+        pass
+
