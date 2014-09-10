d49b3d4cb8193935677faafd6f0c799ccf9b7eb1
Lane <zhanglintc623@gmail.com>
2014-09-10 23:30:15 +0800

    INIT

---
+++ leetcode/python/Linked List Cycle II.py
@@ -0,0 +1,28 @@
+# Linked List Cycle II
+# for leetcode problems
+# 2014.09.10 by zhanglin
+
+# Problem:
+# Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+# Follow up:
+# Can you solve it without using extra space?
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param head, a ListNode
+    # @return a list node
+    def detectCycle(self, head):
+        pass
+
