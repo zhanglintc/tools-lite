a3bacc9e744410adc0cbc656211bb6be072c9a6e
Lane <zhanglintc623@gmail.com>
2014-09-10 23:13:28 +0800

    INIT

---
+++ leetcode/python/Linked List Cycle.py
@@ -0,0 +1,22 @@
+# Linked List Cycle
+# for leetcode problems
+# 2014.09.10 by zhanglin
+
+# Problem:
+# Given a linked list, determine if it has a cycle in it.
+
+# Follow up:
+# Can you solve it without using extra space?
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param head, a ListNode
+    # @return a boolean
+    def hasCycle(self, head):
+        pass
+
