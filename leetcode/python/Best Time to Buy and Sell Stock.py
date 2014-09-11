6daadaaaf6497c14eb53c2a5bac86746cd540fbb
Lane <zhanglintc623@gmail.com>
2014-09-11 18:30:59 +0800

    INIT

---
+++ leetcode/python/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,16 @@
+# Best Time to Buy and Sell Stock
+# for leetcode problems
+# 2014.09.11 by zhanglin
+
+# Problem:
+# Say you have an array for which the i[th] element is the price of a given stock on day i.
+
+# If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock)
+# design an algorithm to find the maximum profit.
+
+class Solution:
+    # @param prices, a list of integer
+    # @return an integer
+    def maxProfit(self, prices):
+        pass
+
