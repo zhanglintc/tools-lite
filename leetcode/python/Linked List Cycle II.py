04cb3a8c8367e25bbdd4a6d3b3297bfc86260cf4
Lane <zhanglintc623@gmail.com>
2014-09-11 11:04:18 +0800

    AC

--- leetcode/python/Linked List Cycle II.py
+++ leetcode/python/Linked List Cycle II.py
@@ -14,15 +14,25 @@
 #         self.val = x
 #         self.next = None
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution:
     # @param head, a ListNode
     # @return a list node
     def detectCycle(self, head):
-        pass
+        fast = head
+        slow = head
+
+        while fast != None and fast.next != None: # while not the end
+            fast = fast.next.next
+            slow = slow.next
+
+            if fast == slow: # there is a circle
+                fast = head # reset fast to head
+
+                while fast != slow: # while not meet
+                    fast = fast.next
+                    slow = slow.next
+
+                return fast # return the meet position
+
+        return None # no circle return None
 
