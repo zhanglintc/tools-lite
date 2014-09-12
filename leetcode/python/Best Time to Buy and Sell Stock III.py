9451c47c270a04ea32986c6e9fd55a091f7066cd
Lane <zhanglintc623@gmail.com>
2014-09-12 09:57:59 +0800

    MIDDLE

--- leetcode/python/Best Time to Buy and Sell Stock III.py
+++ leetcode/python/Best Time to Buy and Sell Stock III.py
@@ -14,7 +14,35 @@ class Solution:
     # @param prices, a list of integer
     # @return an integer
     def maxProfit(self, prices):
-        pass
+        profits = []
+        bought = False
 
+        for i in range(len(prices) - 1):
+            if prices[i] < prices[i + 1] and bought == False:
+                buy = prices[i]
+                bought = True
 
+            if prices[i] > prices[i + 1] and bought == True:
+                sell = prices[i]
+                bought = False
+                profits.append(sell - buy)
+
+        if bought == True:
+            sell = prices[-1]
+            profits.append(sell - buy)
+
+        profits.sort()
+        
+        length = len(profits)
+        if length == 0:
+            profit = 0
+        elif length == 1:
+            profit = profits[-1]
+        else:
+            profit = profits[-2] + profits[-1]
+        print(profits)
+        return profit
+
+s = Solution()
+print (s.maxProfit([1,2,4,2,5,7,2,4,9,0]))
 
