188bc6137022f568ced489f611ef6c6a11eca0af
Lane <zhanglintc623@gmail.com>
2014-09-12 21:53:09 +0800

    TLE
    comment更改

--- leetcode/c++/Best Time to Buy and Sell Stock III.cpp
+++ leetcode/c++/Best Time to Buy and Sell Stock III.cpp
@@ -7,7 +7,7 @@
 
 // Design an algorithm to find the maximum profit. You may complete at most TWO transactions.
 
-// Note1:
+// Note:
 // You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
 
 class Solution
