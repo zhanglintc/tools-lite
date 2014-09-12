ef1f377da014e0b015ca0ae1994fdc1dbe5d5d96
Lane <zhanglintc623@gmail.com>
2014-09-12 23:51:08 +0800

    INIT

---
+++ leetcode/python/Pascal's Triangle.py
@@ -0,0 +1,24 @@
+# Pascal's Triangle
+# for leetcode problems
+# 2014.09.12 by zhanglin
+
+# Problem:
+# Given numRows, generate the first numRows of Pascal's triangle.
+
+# For example, given numRows = 5,
+# Return
+
+# [
+#      [1],
+#     [1,1],
+#    [1,2,1],
+#   [1,3,3,1],
+#  [1,4,6,4,1]
+# ]
+
+class Solution:
+    # @return a list of lists of integers
+    def generate(self, numRows):
+        pass
+
+
