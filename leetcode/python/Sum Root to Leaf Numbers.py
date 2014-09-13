b7b0e2f27de8f2b226abddc0e5340f52b29a47ee
Lane <zhanglintc623@gmail.com>
2014-09-14 02:16:42 +0800

    INIT

---
+++ leetcode/python/Sum Root to Leaf Numbers.py
@@ -0,0 +1,35 @@
+# Sum Root to Leaf Numbers
+# for leetcode problems
+# 2014.09.14 by zhanglin
+
+# Problem:
+# Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+# An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+# Find the total sum of all root-to-leaf numbers.
+
+# For example,
+
+#     1
+#    / \
+#   2   3
+# The root-to-leaf path 1->2 represents the number 12.
+# The root-to-leaf path 1->3 represents the number 13.
+
+# Return the sum = 12 + 13 = 25.
+
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return an integer
+    def sumNumbers(self, root):
+        pass
+
+
