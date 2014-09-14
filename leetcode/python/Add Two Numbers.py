d425c995e7be9d010b038e90733bb59509491162
Lane <zhanglintc623@gmail.com>
2014-09-14 15:03:20 +0800

    AC
    添加看板信息

--- leetcode/python/Add Two Numbers.py
+++ leetcode/python/Add Two Numbers.py
@@ -1,3 +1,15 @@
+# Add Two Numbers
+# for leetcode problems
+# 2014.08.15 by zhanglin
+
+# Problem:
+# You are given two linked lists representing two non-negative numbers.
+# The digits are stored in reverse order and each of their nodes contain a single digit.
+# Add the two numbers and return it as a linked list.
+
+# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
