483f66bf712caf19f04c97411f4c7678c61c6fb5
Lane <zhanglintc623@gmail.com>
2014-09-16 18:17:51 +0800

    INIT

---
+++ leetcode/python/Combinations.py
@@ -0,0 +1,24 @@
+# Combinations
+# for leetcode problems
+# 2014.09.16 by zhanglin
+
+# Problem:
+# Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+
+# For example,
+# If n = 4 and k = 2, a solution is:
+
+# [
+#   [2,4],
+#   [3,4],
+#   [2,3],
+#   [1,2],
+#   [1,3],
+#   [1,4],
+# ]
+
+class Solution:
+    # @return a list of lists of integers
+    def combine(self, n, k):
+        pass
+
