b2162ffb0f47f731b2d89a867bc551ea8988ece4
Lane <zhanglintc623@gmail.com>
2014-09-17 17:57:36 +0800

    AC
    分治算法，但是其中mid部分的算法跟我的想象完全不同

--- leetcode/python/Maximum Subarray.py
+++ leetcode/python/Maximum Subarray.py
@@ -5,8 +5,8 @@
 # Problem:
 # Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
 
-# For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
-# the contiguous subarray [4,−1,2,1] has the largest sum = 6.
+# For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+# the contiguous subarray [4,-1,2,1] has the largest sum = 6.
 
 # More practice:
 # If you have figured out the O(n) solution,
@@ -16,12 +16,32 @@ class Solution:
     # @param A, a list of integers
     # @return an integer
     def maxSubArray(self, A):
-        this_sum = A[0]
-        max_sum  = A[0]
-        for i in range(1, len(A)):
-            this_sum = (A[i] if this_sum < 0 else this_sum + A[i])
-            max_sum  = (this_sum if this_sum > max_sum else max_sum)
+        if len(A) == 0:
+            return None
 
-        return max_sum
+        if len(A) == 1:
+            return A[0]
+
+        mid = len(A) // 2
+        left_max  = self.maxSubArray(A[:mid])
+        right_max = self.maxSubArray(A[mid:])
+
+        mid_max = A[mid - 1]
+        left = A[mid - 1]
+        i = mid - 2
+        while i >= 0:
+            mid_max += A[i]
+            left = max(mid_max, left)
+            i -= 1
+
+        mid_max = A[mid]
+        right = A[mid]
+        i = mid + 1
+        while i < len(A):
+            mid_max += A[i]
+            right = max(mid_max, right)
+            i += 1
+
+        return max(left_max, left + right, right_max)
 
 
