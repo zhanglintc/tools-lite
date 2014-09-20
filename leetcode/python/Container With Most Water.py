7cfc44024197aaa95cadba607cc6417d4b0520f3
Lane <zhanglintc623@gmail.com>
2014-09-20 22:58:21 +0800

    INIT

---
+++ leetcode/python/Container With Most Water.py
@@ -0,0 +1,17 @@
+# Container With Most Water
+# for leetcode problems
+# 2014.09.20 by zhanglin
+
+# Problem:
+# Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai).
+# n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+# Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+# Note: You may not slant the container.
+
+class Solution:
+    # @return an integer
+    def maxArea(self, height):
+        pass
+
+
