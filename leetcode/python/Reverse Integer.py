30522552812e13649e056e51ba12bd861436235a
Lane <zhanglintc623@gmail.com>
2014-09-20 22:47:41 +0800

    AC
    添加问题内容
    删除测试代码

--- leetcode/python/Reverse Integer.py
+++ leetcode/python/Reverse Integer.py
@@ -2,6 +2,24 @@
 # for leetcode problems
 # 2014.08.20 by zhanglin
 
+# Problem:
+# Reverse digits of an integer.
+
+# Example1: x = 123, return 321
+# Example2: x = -123, return -321
+
+# Have you thought about this?
+# Here are some good questions to ask before coding. 
+# Bonus points for you if you have already thought through this!
+
+# If the integer's last digit is 0, what should the output be? ie, cases such as 10, 100.
+
+# Did you notice that the reversed integer might overflow? Assume the input is a 32-bit integer,
+# then the reverse of 1000000003 overflows. How should you handle such cases?
+
+# Throw an exception? Good, but what if throwing an exception is not an option?
+# You would then have to re-design the function (ie, add an extra parameter).
+
 class Solution:
     # @return an integer
     def reverse(self, x):
@@ -26,6 +44,3 @@ class Solution:
         return x * sign
 
 
-input = 10100
-S = Solution()
-print(S.reverse(input))
