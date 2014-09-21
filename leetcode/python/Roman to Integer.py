6340b98171a815be73ee890c929dc6edc233509e
Lane <zhanglintc623@gmail.com>
2014-09-21 23:39:50 +0800

    INIT

---
+++ leetcode/python/Roman to Integer.py
@@ -0,0 +1,22 @@
+# Roman to Integer
+# for leetcode problems
+# 2014.09.21 by zhanglin
+
+# Problem:
+# Given a roman numeral, convert it to an integer.
+
+# Input is guaranteed to be within the range from 1 to 3999.
+
+class Solution:
+    # @return an integer
+    def romanToInt(self, s):
+        convert_list = [
+            ['','I','II','III','IV','V','VI','VII','VIII','IX'],
+            ['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC'],
+            ['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM'],
+            ['','M','MM','MMM']
+        ]
+
+        pass
+
+
