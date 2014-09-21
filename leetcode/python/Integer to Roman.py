1e9c095188d20fc625102bb4f2f51a43bdd7a521
Lane <zhanglintc623@gmail.com>
2014-09-21 21:25:53 +0800

    INIT

---
+++ leetcode/python/Integer to Roman.py
@@ -0,0 +1,15 @@
+# Integer to Roman
+# for leetcode problems
+# 2014.09.21 by zhanglin
+
+# Problem:
+# Given an integer, convert it to a roman numeral.
+
+# Input is guaranteed to be within the range from 1 to 3999.
+
+class Solution:
+    # @return a string
+    def intToRoman(self, num):
+        pass
+
+
