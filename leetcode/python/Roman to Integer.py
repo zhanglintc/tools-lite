4a7bb821f1ac83d4446d8978200ccbe5b968b33e
Lane <zhanglintc623@gmail.com>
2014-09-22 18:40:41 +0800

    MIDDLE

--- leetcode/python/Roman to Integer.py
+++ leetcode/python/Roman to Integer.py
@@ -10,13 +10,30 @@
 class Solution:
     # @return an integer
     def romanToInt(self, s):
-        convert_list = [
-            ['','I','II','III','IV','V','VI','VII','VIII','IX'],
-            ['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC'],
-            ['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM'],
-            ['','M','MM','MMM']
-        ]
+        convert_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
 
-        pass
+        s.reverse()
+        last = 'I'
+        digit = 0
+        for i in s:
+            if convert_dict[i] < convert_dict[last]:
+                digit -= convert_dict[i]
+            else:
+                digit += convert_dict[i]
+
+            last = i
+
+        return digit
+
+s = Solution()
+print(s.romanToInt("X"))
 
 
