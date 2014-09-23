db9ec813908b7454260c0d64920904a51073d5f1
Lane <zhanglintc623@gmail.com>
2014-09-24 01:08:07 +0800

    INIT

---
+++ leetcode/python/Permutations II.py
@@ -0,0 +1,19 @@
+# Permutations II
+# for leetcode problems
+# 2014.09.24 by zhanglin
+
+# Problem:
+# Given a collection of numbers that might contain duplicates,
+# return all possible UNIQUE permutations.
+
+# For example,
+# [1,1,2] have the following unique permutations:
+# [1,1,2], [1,2,1], and [2,1,1].
+
+class Solution:
+    # @param num, a list of integer
+    # @return a list of lists of integers
+    def permuteUnique(self, num):
+        pass
+
+
