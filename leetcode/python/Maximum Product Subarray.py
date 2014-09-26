0e7b566f47fae43c12bfe277aa98b9ab62e3576d
Lane <zhanglintc623@gmail.com>
2014-09-26 12:53:25 +0800

    AC
    优化代码

--- leetcode/python/Maximum Product Subarray.py
+++ leetcode/python/Maximum Product Subarray.py
@@ -8,34 +8,24 @@
 # For example, given the array [2,3,-2,4],
 # the contiguous subarray [2,3] has the largest product = 6.
 
+# O(2n) time
 class Solution:
     # @param A, a list of integers
     # @return an integer
     def maxProduct(self, A):
+        # initialize
         left  = 0
         right = 0
         fina_max = A[0]
         
-        for i in A:
-            if i != 0:
-                if left == 0:
-                    left = i
-                else:
-                    left = left * i
-            else:
-                left = 0
-
+        # from left to right
+        for i in A: 
+            left = (left * i if left != 0 else i)
             fina_max = max(left, fina_max)
-            
+        
+        # from right to left
         for i in A[::-1]:
-            if i != 0:
-                if right == 0:
-                    right = i
-                else:
-                    right = right * i
-            else:
-                right = 0
-
+            right = (right * i if right != 0 else i)
             fina_max = max(right, fina_max)
 
         return fina_max
