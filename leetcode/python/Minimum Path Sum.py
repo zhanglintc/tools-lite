af43f1bbd2d0633daca3771969997ed10543a3d0
Lane <zhanglintc623@gmail.com>
2014-09-26 22:59:44 +0800

    INIT

---
+++ leetcode/python/Minimum Path Sum.py
@@ -0,0 +1,17 @@
+# Minimum Path Sum
+# for leetcode problems
+# 2014.09.26 by zhanglin
+
+# Problem:
+# Given a m x n grid filled with non-negative numbers,
+# find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+# Note: You can only move either down or right at any point in time.
+
+class Solution:
+    # @param grid, a list of lists of integers
+    # @return an integer
+    def minPathSum(self, grid):
+        pass
+
+
