63f38ae8bb11ce0f066ab5c650d663c2d2e9ac05
Lane <zhanglintc623@gmail.com>
2014-09-27 18:53:18 +0800

    INIT

---
+++ leetcode/python/Merge Two Sorted Lists.py
@@ -0,0 +1,21 @@
+# Merge Two Sorted Lists
+# for leetcode problems
+# 2014.09.27 by zhanglin
+
+# Problem:
+# Merge two sorted linked lists and return it as a new list.
+# The new list should be made by splicing together the nodes of the first two lists.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param two ListNodes
+    # @return a ListNode
+    def mergeTwoLists(self, l1, l2):
+        pass
+
+
