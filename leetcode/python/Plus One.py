19baa8371ad7431e07c0edb9f28e3d94247b300e
Lane <zhanglintc623@gmail.com>
2014-09-27 22:06:05 +0800

    INIT

---
+++ leetcode/python/Plus One.py
@@ -0,0 +1,16 @@
+# Plus One
+# for leetcode problems
+# 2014.09.27 by zhanglin
+
+# Problem:
+# Given a non-negative number represented as an array of digits, plus one to the number.
+
+# The digits are stored such that the most significant digit is at the head of the list.
+
+class Solution:
+    # @param digits, a list of integer digits
+    # @return a list of integer digits
+    def plusOne(self, digits):
+        pass
+
+
