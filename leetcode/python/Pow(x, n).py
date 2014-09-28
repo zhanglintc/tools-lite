41e44385aaa35992eb99e170657468c59eed4a94
Lane <zhanglintc623@gmail.com>
2014-09-28 23:14:09 +0800

    INIT

---
+++ leetcode/python/Pow(x, n).py
@@ -0,0 +1,15 @@
+# Pow(x, n)
+# for leetcode problems
+# 2014.09.28 by zhanglin
+
+# Problem:
+# Implement pow(x, n).
+
+class Solution:
+    # @param x, a float
+    # @param n, a integer
+    # @return a float
+    def pow(self, x, n):
+        pass
+
+
