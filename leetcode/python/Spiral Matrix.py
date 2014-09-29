ee5559a97c875056e04b101228ee5580206e670a
Lane <zhanglintc623@gmail.com>
2014-09-29 23:44:40 +0800

    AC
    先模仿，再创新...

--- leetcode/python/Spiral Matrix.py
+++ leetcode/python/Spiral Matrix.py
@@ -21,39 +21,42 @@ class Solution:
     def spiralOrder(self, matrix):
         if not matrix:
             return matrix
+
         spiralTraversal = []
-        self.spiralOrder_helper(matrix, spiralTraversal, 0, len(matrix), 0, len(matrix[0]))
+        direction = 0 # 0 for left->right, 1 for up->down, 2 for right->left, 3 for down->up
+
+        row_start  = 0
+        row_end    = len(matrix)
+        line_start = 0
+        line_end   = len(matrix[0])
+
+        while True:
+            if direction == 0:
+                for i in range(line_start, line_end):
+                    spiralTraversal.append(matrix[row_start][i])
+                row_start += 1 # remove first row
+
+            if direction == 1:
+                for i in range(row_start, row_end):
+                    spiralTraversal.append(matrix[i][line_end - 1])
+                line_end -= 1 # remove last line
+
+            if direction == 2:
+                for i in range(line_end - 1, line_start - 1, -1): # -1 for offset
+                    spiralTraversal.append(matrix[row_end - 1][i])
+                row_end -= 1 # remove last row
+
+            if direction == 3:
+                for i in range(row_end - 1, row_start - 1, -1): # -1 for offset
+                    spiralTraversal.append(matrix[i][line_start])
+                line_start += 1 # remove first line
+
+            if row_start == row_end or line_start == line_end: # use 'or' no matter row or line meets
+                break # break if meets
+
+            direction += 1 # change dirction
+            direction %= 4 # reset if overflow
+
         return spiralTraversal
-
-    def spiralOrder_helper(self, matrix, spiralTraversal, row_start, row_end, line_start, line_end):
-        if row_start == row_end - 1 and line_start == line_end - 1:
-            spiralTraversal.append(matrix[row_start][line_start])
-            return
-
-        for i in range(line_start, line_end - 1): # left -> right
-            spiralTraversal.append(matrix[0][i])
-
-        for i in range(row_start, row_end - 1): # up -> down
-            spiralTraversal.append(matrix[i][line_end - 1])
-
-        for i in range(line_end - 1, line_start, -1): # right -> left
-            spiralTraversal.append(matrix[row_end - 1][i])
-
-        for i in range(row_end - 1, row_start, -1): # down -> up
-            spiralTraversal.append(matrix[i][0])
-
-        self.spiralOrder_helper(matrix, spiralTraversal, row_start + 1, row_end - 1, line_start + 1, line_end - 1)
-
-
-
-
-
-lst = [
- [ 1, 2, 3 ],
- [ 4, 5, 6 ],
- [ 7, 8, 9 ]
-]
-s = Solution()
-print(s.spiralOrder(lst))
 
 
