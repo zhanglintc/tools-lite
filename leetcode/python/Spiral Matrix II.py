0b314f783966115ea6fdd16716cef425e13166ff
Lane <zhanglintc623@gmail.com>
2014-09-30 01:03:39 +0800

    AC
    可以略作修改

--- leetcode/python/Spiral Matrix II.py
+++ leetcode/python/Spiral Matrix II.py
@@ -18,6 +18,46 @@
 class Solution:
     # @return a list of lists of integer
     def generateMatrix(self, n):
-        pass
+        if not n:
+            return []
+
+        matrix = [[None for i in range(n)] for i in range(n)]
+
+        spiralTraversal = range(1, n ** 2 + 1)
+        direction = 0 # 0 for left->right, 1 for up->down, 2 for right->left, 3 for down->up
+
+        row_start  = 0
+        row_end    = len(matrix)
+        line_start = 0
+        line_end   = len(matrix[0])
+
+        while True:
+            if direction == 0:
+                for i in range(line_start, line_end):
+                    matrix[row_start][i] = spiralTraversal.pop(0)
+                row_start += 1 # remove first row
+
+            if direction == 1:
+                for i in range(row_start, row_end):
+                    matrix[i][line_end - 1] = spiralTraversal.pop(0)
+                line_end -= 1 # remove last line
+
+            if direction == 2:
+                for i in range(line_end - 1, line_start - 1, -1): # -1 for offset
+                    matrix[row_end - 1][i] = spiralTraversal.pop(0)
+                row_end -= 1 # remove last row
+
+            if direction == 3:
+                for i in range(row_end - 1, row_start - 1, -1): # -1 for offset
+                    matrix[i][line_start] = spiralTraversal.pop(0)
+                line_start += 1 # remove first line
+
+            if row_start == row_end or line_start == line_end: # use 'or' no matter row or line meets
+                break # break if meets
+
+            direction += 1 # change dirction
+            direction %= 4 # reset if overflow
+
+        return matrix
 
 
