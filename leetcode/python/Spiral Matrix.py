6b13e99c509f12c6ede109b50bb79087dbc5ec9e
Lane <zhanglintc623@gmail.com>
2014-09-29 22:35:20 +0800

    MIDDLE
    完全不知所云啊

--- leetcode/python/Spiral Matrix.py
+++ leetcode/python/Spiral Matrix.py
@@ -19,11 +19,30 @@ class Solution:
     # @param matrix, a list of lists of integers
     # @return a list of integers
     def spiralOrder(self, matrix):
-        pass
+        if not matrix:
+            return matrix
+        spiralTraversal = []
+        self.spiralOrder_helper(matrix, spiralTraversal, 0, len(matrix), 0, len(matrix[0]))
+        return spiralTraversal
 
-    def spiralOrder_helper(self, matrix, m, n):
-        pass
+    def spiralOrder_helper(self, matrix, spiralTraversal, row_start, row_end, line_start, line_end):
+        if row_start == row_end - 1 and line_start == line_end - 1:
+            spiralTraversal.append(matrix[row_start][line_start])
+            return
 
+        for i in range(line_start, line_end - 1): # left -> right
+            spiralTraversal.append(matrix[0][i])
+
+        for i in range(row_start, row_end - 1): # up -> down
+            spiralTraversal.append(matrix[i][line_end - 1])
+
+        for i in range(line_end - 1, line_start, -1): # right -> left
+            spiralTraversal.append(matrix[row_end - 1][i])
+
+        for i in range(row_end - 1, row_start, -1): # down -> up
+            spiralTraversal.append(matrix[i][0])
+
+        self.spiralOrder_helper(matrix, spiralTraversal, row_start + 1, row_end - 1, line_start + 1, line_end - 1)
 
 
 
