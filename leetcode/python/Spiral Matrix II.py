ff39825f22beaba513ef707fb0a4976535e3ad9c
Lane <zhanglintc623@gmail.com>
2014-09-29 23:47:00 +0800

    INIT

---
+++ leetcode/python/Spiral Matrix II.py
@@ -0,0 +1,23 @@
+# Spiral Matrix II
+# for leetcode problems
+# 2014.09.29 by zhanglin
+
+# Problem:
+# Given an integer n, generate a square matrix filled with elements from 1 to n^2 in spiral order.
+
+# For example,
+# Given n = 3,
+
+# You should return the following matrix:
+# [
+#  [ 1, 2, 3 ],
+#  [ 8, 9, 4 ],
+#  [ 7, 6, 5 ]
+# ]
+
+class Solution:
+    # @return a list of lists of integer
+    def generateMatrix(self, n):
+        pass
+
+
