096f5ff206d80bfbca605cc1fffe83568b20e072
Lane <zhanglintc623@gmail.com>
2014-10-01 00:12:34 +0800

    MIDDLE

--- leetcode/python/Search a 2D Matrix.py
+++ leetcode/python/Search a 2D Matrix.py
@@ -23,6 +23,24 @@ class Solution:
     # @param target, an integer
     # @return a boolean
     def searchMatrix(self, matrix, target):
-        pass
+        if len(matrix) == 0:
+            return False
+
+        if len(matrix) == 1:
+            return (True if matrix[0] == target else False)
+
+        mid = len(matrix) // 2
+        if matrix[mid] == target:
+            return True
+
+        else:
+            return (
+                self.searchMatrix(matrix[:mid], target)
+                if target < matrix[mid] else
+                self.searchMatrix(matrix[mid+1:], target)
+                )
+
+s = Solution()
+print s.searchMatrix([1,2,3,4,5,6.5], 6.5)
 
 
