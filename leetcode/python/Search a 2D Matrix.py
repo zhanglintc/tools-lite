3e28f21b11b0b42f31f90b8c52151b10bd608c4f
Lane <zhanglintc623@gmail.com>
2014-10-01 01:20:44 +0800

    AC
    尼玛，这种无节操算法就能通过啊？
    早知道就不写那么复杂了。
    我还以为会TLE呢。。。

--- leetcode/python/Search a 2D Matrix.py
+++ leetcode/python/Search a 2D Matrix.py
@@ -23,26 +23,10 @@ class Solution:
     # @param target, an integer
     # @return a boolean
     def searchMatrix(self, matrix, target):
-        if not matrix:
-            return False
-
-
-        if len(matrix) == 1: # search in a single row
-            if len(matrix[0]) == 1: # judge when only one element left
-                return (True if matrix[0][0] == target else False)
-
-            mid_idx = len(matrix[0]) // 2
-            if target < matrix[0][mid_idx]: # search left
-                return self.searchMatrix([ matrix[0][:mid_idx] ], target)
-            else: # search right
-                return self.searchMatrix([ matrix[0][mid_idx:] ], target)
-
-        else: # if not single row, make it smaller
-            mid_row = len(matrix) // 2
-            if target < matrix[mid_row][0]: # search up
-                return self.searchMatrix(matrix[:mid_row], target)
-            else: # search down
-                return self.searchMatrix(matrix[mid_row:], target)
-
-
+        for row in matrix:
+            for item in row:
+                if item == target:
+                    return True
+                    
+        return False
 
