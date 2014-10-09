d3e77612377765400ae4e4c117b578d02fd3b6cf
Lane <zhanglintc623@gmail.com>
2014-10-09 18:10:26 +0800

    AC
    添加括号

--- leetcode/python/Regular Expression Matching.py
+++ leetcode/python/Regular Expression Matching.py
@@ -38,7 +38,7 @@ class Solution:
         for i in range(1, len(s) + 1):
             for j in range(1, len(p) + 1):
                 if p[j - 1] == '*':
-                    dp[i][j] = (dp[i][j - 1] or dp[i][j - 2]) or dp[i - 1][j] and (s[i - 1] == p[j - 2] or p[j - 2] == '.')
+                    dp[i][j] = (dp[i][j - 1] or dp[i][j - 2]) or (dp[i - 1][j] and (s[i - 1] == p[j - 2] or p[j - 2] == '.'))
 
                 else:
                     dp[i][j] = dp[i - 1][j - 1] and (s[i - 1] == p[j - 1] or p[j - 1] == '.')
