af8f95da34a0afe43192fee16747383377f86b54
Lane <zhanglintc623@gmail.com>
2014-10-11 11:31:06 +0800

    minors fix

--- misc/weiboClient/weibo_lite.py
+++ misc/weiboClient/weibo_lite.py
@@ -2,6 +2,8 @@ from weibo import Client
 import json
 import time
 
+DELAY_TIME = 0
+
 API_KEY = '2038131539'            # app key
 API_SECRET = 'b4d84f59af3e5a52c8df1f0e7ccfa75d'      # app secret
 REDIRECT_URI = 'http://zhanglintc.blog.163.com'  # callback ur
@@ -17,33 +19,32 @@ token = {u'access_token': u'2.00_ShHyB2HnvNCca83c087d04aFAkC', u'remind_in': u'1
 
 fw = open('comments.txt', 'ab')
 
-print 'begin1'
 c2 = Client(API_KEY, API_SECRET, REDIRECT_URI, token)
-print 'begin2'
 
 my_page = 1
-while my_page <= 267:
+while my_page <= 25:
     try:
-        received = c2.get('comments/to_me', uid = 1804547715, page = my_page)
-        print 'page', my_page, 'downloading'
+        print('page', my_page, 'downloading')
+        received = c2.get('comments/to_me', uid = 1804547715, page = my_page)        
 
     except:
-        print 'page', my_page, 'failed'
-        time.sleep(5)
+        print('page', my_page, 'has failed\n')
+        time.sleep(DELAY_TIME)
         continue
 
-    fw.write('\n\npage' + str(my_page) + ':\n')
+    fw.write(b'\n\npage' + str(my_page).encode('utf8') + b':\n')
     for item in received['comments']:
-        to_be_written = item['created_at'] + item['text'] + ' by ' + item['user']['name'] + '\n'
+        to_be_written = item['created_at'] + ': ' + item['text'] + ' by ' + item['user']['name'] + '\n'
         fw.write(to_be_written.encode('utf8'))
 
-    print 'page', my_page, 'downloaded'
-    time.sleep(5)
+    time.sleep(DELAY_TIME)
+    fw.flush()
+    print('page', my_page, 'downloaded\n')
     my_page += 1
-
+    
 fw.close()
 
-print 'all the comments have downloaded'
+print('All the comments have downloaded')
 
 
 
