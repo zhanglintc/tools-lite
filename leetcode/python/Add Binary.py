db97ef0fc56014335a49e974d39c8ae8cd19c766
Lane <zhanglintc623@gmail.com>
2014-10-19 23:22:11 +0800

    INIT

---
+++ leetcode/python/Add Binary.py
@@ -0,0 +1,20 @@
+# Add Binary
+# for leetcode problems
+# 2014.10.19 by zhanglin
+
+# Problem:
+# Given two binary strings, return their sum (also a binary string).
+
+# For example,
+# a = "11"
+# b = "1"
+# Return "100".
+
+class Solution:
+    # @param a, a string
+    # @param b, a string
+    # @return a string
+    def addBinary(self, a, b):
+        pass
+
+
