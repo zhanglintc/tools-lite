d640d33a5ef49c633ca9ac6c91e2f0477b067c8c
Lane <zhanglintc623@gmail.com>
2014-10-19 12:42:44 +0800

    INIT

---
+++ leetcode/python/Valid Sudoku.py
@@ -0,0 +1,25 @@
+# Valid Sudoku
+# for leetcode problems
+# 2014.10.19 by zhanglin
+
+# Problem:
+
+# Refer to:
+# https://oj.leetcode.com/problems/valid-sudoku/
+
+# Determine if a Sudoku is valid.
+
+# The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+# A partially filled sudoku which is valid.
+
+# Note:
+# A valid Sudoku board (partially filled) is not necessarily solvable. Only the filled cells need to be validated.
+
+class Solution:
+    # @param board, a 9x9 2D array
+    # @return a boolean
+    def isValidSudoku(self, board):
+        pass
+
+
