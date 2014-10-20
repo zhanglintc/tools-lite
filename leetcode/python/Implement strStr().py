6aa836e2329e1bb7ec76ef71fe28808b0ddae6d6
Lane <zhanglintc623@gmail.com>
2014-10-20 17:34:57 +0800

    INIT

---
+++ leetcode/python/Implement strStr().py
@@ -0,0 +1,17 @@
+# Implement strStr()
+# for leetcode problems
+# 2014.10.20 by zhanglin
+
+# Problem:
+# Implement strStr().
+
+# Returns a pointer to the first occurrence of needle in haystack, or null if needle is not part of haystack.
+
+class Solution:
+    # @param haystack, a string
+    # @param needle, a string
+    # @return a string or None
+    def strStr(self, haystack, needle):
+        pass
+
+
