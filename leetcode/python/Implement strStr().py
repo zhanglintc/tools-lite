38601a50170936cb4c93516baee1c23ed91aaaa9
Lane <zhanglintc623@gmail.com>
2014-10-23 00:08:15 +0800

    AC
    暴力匹配。
    百度的时候说有KMP算法，具体查明白以后尝试KMP实现。

--- leetcode/python/Implement strStr().py
+++ leetcode/python/Implement strStr().py
@@ -12,6 +12,30 @@ class Solution:
     # @param needle, a string
     # @return a string or None
     def strStr(self, haystack, needle):
-        pass
+        saved_hay = 0
+        cur_hay   = 0
+        cur_ndl   = 0
+
+        # while no one meets the end
+        while cur_hay < len(haystack) and cur_ndl < len(needle):
+            # if not matched, reset cur_ndl but saved_hay cur_hay move forward
+            if haystack[cur_hay] != needle[cur_ndl]:
+                saved_hay += 1
+
+                cur_hay = saved_hay
+                cur_ndl = 0
+
+            # if matched, both cur_hay and cur_ndl move forward
+            else:
+                cur_hay += 1
+                cur_ndl += 1
+
+        # if cur_ndl meets the end, means needle in the haystack
+        if cur_ndl == len(needle):
+            return haystack[saved_hay:]
+
+        # else needle not in the haystack
+        else:
+            return None
 
 
