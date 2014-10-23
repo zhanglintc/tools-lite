f56b66ea5a625cf636e63ec48fa39cf03d3bb408
Lane <zhanglintc623@gmail.com>
2014-10-24 00:25:24 +0800

    INIT

---
+++ leetcode/python/Subsets.py
@@ -0,0 +1,31 @@
+# Subsets
+# for leetcode problems
+# 2014.10.24 by zhanglin
+
+# Problem:
+# Given a set of distinct integers, S, return all possible subsets.
+
+# Note:
+# Elements in a subset must be in non-descending order.
+# The solution set must not contain duplicate subsets.
+# For example,
+# If S = [1,2,3], a solution is:
+
+# [
+#   [3],
+#   [1],
+#   [2],
+#   [1,2,3],
+#   [1,3],
+#   [2,3],
+#   [1,2],
+#   []
+# ]
+
+class Solution:
+    # @param S, a list of integer
+    # @return a list of lists of integer
+    def subsets(self, S):
+        pass
+
+
