8d52cee9150d7f47e5653b7fc2d24ff12b9760b0
Lane <zhanglintc623@gmail.com>
2014-10-25 15:46:36 +0800

    MIDDLE
    #BUG

--- leetcode/python/Subsets.py
+++ leetcode/python/Subsets.py
@@ -26,6 +26,24 @@ class Solution:
     # @param S, a list of integer
     # @return a list of lists of integer
     def subsets(self, S):
-        pass
+        S.sort()
+        fina_lst = []
+        this_lst = [None for i in range(len(S))]
 
+        for req_depth in range(0, len(S) + 1):
+            self.subsets_helper(S, req_depth, 0, 0, fina_lst, this_lst)
+
+        return fina_lst
+
+    def subsets_helper(self, S, req_depth, this_depth, start, fina_lst, this_lst):
+        if this_depth == req_depth:
+            fina_lst.append(this_lst[:req_depth])
+            return
+
+        for i in range(start, len(S)):
+            this_lst[this_depth] = S[i]
+            self.subsets_helper(S, req_depth, this_depth + 1, start + 1, fina_lst, this_lst)
+
+s = Solution()
+print s.subsets([1,2,3])
 
