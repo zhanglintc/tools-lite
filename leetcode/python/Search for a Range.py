9a119d437a960ff354618142cb1168369b7504db
Lane <zhanglintc623@gmail.com>
2014-10-27 17:22:09 +0800

    AC
    time O(n) solution

--- leetcode/python/Search for a Range.py
+++ leetcode/python/Search for a Range.py
@@ -13,11 +13,21 @@
 # Given [5, 7, 7, 8, 8, 10] and target value 8,
 # return [3, 4].
 
+# Time O(n) solution
 class Solution:
     # @param A, a list of integers
     # @param target, an integer to be searched
     # @return a list of length 2, [index1, index2]
     def searchRange(self, A, target):
-        pass
+        start = end = -1
+
+        for i in range(len(A)):
+            if A[i] == target and start == -1:
+                start = i
+
+            if A[i] == target:
+                end = i
+
+        return [start, end]
 
 
