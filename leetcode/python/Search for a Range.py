34d5b8621b541afd7dde8baadfef831747be8677
Lane <zhanglintc623@gmail.com>
2014-10-27 17:12:57 +0800

    INIT

---
+++ leetcode/python/Search for a Range.py
@@ -0,0 +1,23 @@
+# Search for a Range
+# for leetcode problems
+# 2014.10.27 by zhanglin
+
+# Problem:
+# Given a sorted array of integers, find the starting and ending position of a given target value.
+
+# Your algorithm's runtime complexity must be in the order of O(log n).
+
+# If the target is not found in the array, return [-1, -1].
+
+# For example,
+# Given [5, 7, 7, 8, 8, 10] and target value 8,
+# return [3, 4].
+
+class Solution:
+    # @param A, a list of integers
+    # @param target, an integer to be searched
+    # @return a list of length 2, [index1, index2]
+    def searchRange(self, A, target):
+        pass
+
+
