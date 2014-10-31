4f2f2495fc457e018f78fa9f1125d5d9c11b246d
Lane <zhanglintc623@gmail.com>
2014-10-31 22:25:57 +0800

    AC

--- leetcode/python/Trapping Rain Water.py
+++ leetcode/python/Trapping Rain Water.py
@@ -24,8 +24,26 @@ class Solution:
     # @param A, a list of integers
     # @return an integer
     def trap(self, A):
-        # find every position which bigger than left and right first
-        # then? I don't know ...
-        pass
+        if A == []:
+            return 0
+
+        water = 0 # the water can be trapped
+        max_idx = A.index(max(A)) # index of highest plank
+
+        sub_max = 0
+        for i in range(max_idx): # from left to highest
+            if A[i] < sub_max 
+                water += sub_max - A[i]
+            else:
+                sub_max = A[i]
+
+        sub_max = 0
+        for i in range(len(A) - 1, max_idx, -1): # from right to highest
+            if A[i] < sub_max 
+                water += sub_max - A[i]
+            else:
+                sub_max = A[i]
+
+        return water
 
 
