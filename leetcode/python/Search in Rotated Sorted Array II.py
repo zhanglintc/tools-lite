1cde3a955e60ed35be1ad8c69c04e8b3a7658c18
Lane <zhanglintc623@gmail.com>
2014-11-05 00:28:20 +0800

    INIT

---
+++ leetcode/python/Search in Rotated Sorted Array II.py
@@ -0,0 +1,20 @@
+# Search in Rotated Sorted Array II
+# for leetcode problems
+# 2014.11.05 by zhanglin
+
+# Problem:
+# Follow up for "Search in Rotated Sorted Array":
+# What if duplicates are allowed?
+
+# Would this affect the run-time complexity? How and why?
+
+# Write a function to determine if a given target is in the array.
+
+class Solution:
+    # @param A a list of integers
+    # @param target an integer
+    # @return a boolean
+    def search(self, A, target):
+        pass
+
+
