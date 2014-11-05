94c6e14eff8ae989ad4e59d74c8f6c4d8a67a221
Lane <zhanglintc623@gmail.com>
2014-11-05 21:53:38 +0800

    INIT

---
+++ leetcode/python/Jump Game.py
@@ -0,0 +1,23 @@
+# Jump Game
+# for leetcode problems
+# 2014.10.19 by zhanglin
+
+# Problem:
+# Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+# Each element in the array represents your maximum jump length at that position.
+
+# Determine if you are able to reach the last index.
+
+# For example:
+# A = [2,3,1,1,4], return true.
+
+# A = [3,2,1,0,4], return false.
+
+class Solution:
+    # @param A, a list of integers
+    # @return a boolean
+    def canJump(self, A):
+        pass
+
+
