5f096d02d90e87200ac027feaeb146c631100863
Lane <zhanglintc623@gmail.com>
2014-11-07 17:33:26 +0800

    TLE

--- leetcode/python/Jump Game.py
+++ leetcode/python/Jump Game.py
@@ -1,6 +1,6 @@
 # Jump Game
 # for leetcode problems
-# 2014.10.19 by zhanglin
+# 2014.11.05 by zhanglin
 
 # Problem:
 # Given an array of non-negative integers, you are initially positioned at the first index of the array.
@@ -18,6 +18,21 @@ class Solution:
     # @param A, a list of integers
     # @return a boolean
     def canJump(self, A):
-        pass
+        bool_list = [False for i in range(len(A))]
+        bool_list[-1] = True
+
+        for i in range(len(A) - 2, -1, -1):
+            for step in range(1, A[i] + 1):
+                if bool_list[i + step] == True:
+                    bool_list[i] = True
+                    break
+
+        if bool_list[0] == True:
+            return True
+        else:
+            return False
+
+s = Solution()
+print s.canJump([1,2,0,1,4])
 
 
