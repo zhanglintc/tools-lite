9434779fb9771869f610b6138c27bb6270711337
Lane <zhanglintc623@gmail.com>
2014-11-20 18:04:45 +0800

    INIT

---
+++ leetcode/python/3Sum Closest.py
@@ -0,0 +1,18 @@
+# 3Sum Closest
+# for leetcode problems
+# 2014.11.20 by zhanglin
+
+# Problem:
+# Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target.
+# Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+# For example, given array S = {-1 2 1 -4}, and target = 1.
+
+# The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+class Solution:
+    # @return an integer
+    def threeSumClosest(self, num, target):
+        pass
+
+
