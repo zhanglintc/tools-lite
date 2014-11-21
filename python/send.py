6ce226d5245e1a455bd5ca3ea52e8a977e8e5d01
Lane <zhanglintc623@gmail.com>
2014-11-21 11:19:12 +0800

    format

--- python/send.py
+++ python/send.py
@@ -1,12 +1,23 @@
 import os
 
-title = '"在哪儿，来不来？"'
-body = '"你回家了？"'
-address = "0801jcjhz@163.com"
-username = "zhanglintc@163.com"
-password = "l0velin7211"
+title = '"Send from blat tool"'
+body = '"xxxxxxxxx"'
+address = "xxxxxxxxx@xxxx.com"
+username = "xxxxxxxxx@163.com"
+password = "xxxxxxxxx"
 times = 1
 
-os.system("blat -install smtp.163.com zhanglintc@163.com 3 25")
+os.system("blat -install smtp.163.com xxxxxxxxx@163.com 3 25")
 for i in range(times):
-    os.system("blat -body " + body + " -to " + address + " -s "+title +" -u " + username + " -pw "+ password)
+    # os.system("blat -body " + body + " -to " + address + " -s "+ title +" -u " + username + " -pw "+ password)
+    os.system\
+        (
+            "blat -body {} -to {} -s {} -u {} -pw {}".format\
+                (
+                    body,
+                    address,
+                    title,
+                    username,
+                    password,
+                )
+        )
