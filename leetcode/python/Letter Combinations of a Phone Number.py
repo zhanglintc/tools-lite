dec035fd3c2ccba76c0effff09469228b30e7a80
Lane <zhanglintc623@gmail.com>
2014-11-26 18:27:27 +0800

    MIDDLE

--- leetcode/python/Letter Combinations of a Phone Number.py
+++ leetcode/python/Letter Combinations of a Phone Number.py
@@ -30,16 +30,13 @@ class Solution:
     # @return a list of strings, [s1, s2]
     def letterCombinations(self, digits):
         telephone = {
-            '1': '',
-            '2': 'abc',
-            '3': 'abc',
-            '4': 'abc',
-            '5': 'abc',
-            '6': 'abc',
-            '7': 'abc',
-            '8': 'abc',
-            '9': 'abc',
+            '1': ''    , '2': 'abc', '3': 'def' ,
+            '4': 'ghi' , '5': 'jkl', '6': 'mno' ,
+            '7': 'pqrs', '8': 'tuv', '9': 'wxyz',
             '0': '',
         }
+
+    def helper():
+        pass
 
 
