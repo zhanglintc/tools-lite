f7e6ad69d3f5f21448e68851631a2f6a29c47d37
Lane <zhanglintc623@gmail.com>
2014-11-26 23:12:12 +0800

    INIT

---
+++ leetcode/python/Reverse Linked List II.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Reverse Linked List II
+# for leetcode problems
+# 2014.11.26 by zhanglin
+
+# Problem:
+# Reverse a linked list from position m to n. Do it in-place and in one-pass.
+
+# For example:
+# Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+
+# return 1->4->3->2->5->NULL.
+
+# Note:
+# Given m, n satisfy the following condition:
+# 1 ≤ m ≤ n ≤ length of list.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param head, a ListNode
+    # @param m, an integer
+    # @param n, an integer
+    # @return a ListNode
+    def reverseBetween(self, head, m, n):
+        pass
+
+
