a616c7cc189bb8b241e10a8233d632e25a94b44d
Lane <zhanglintc623@gmail.com>
2014-11-26 23:02:28 +0800

    AC
    one try and pass

--- leetcode/python/Letter Combinations of a Phone Number.py
+++ leetcode/python/Letter Combinations of a Phone Number.py
@@ -29,14 +29,31 @@
 class Solution:
     # @return a list of strings, [s1, s2]
     def letterCombinations(self, digits):
-        telephone = {
+        fina_lst = []
+
+        self.helper(digits, '', fina_lst)
+
+        return fina_lst
+
+    def helper(self, digits, this_str, fina_lst):
+        conv_map = {
             '1': ''    , '2': 'abc', '3': 'def' ,
             '4': 'ghi' , '5': 'jkl', '6': 'mno' ,
             '7': 'pqrs', '8': 'tuv', '9': 'wxyz',
             '0': '',
         }
 
-    def helper():
-        pass
+        # if all digit in digits is used, append the combination to the fina_lst
+        if not digits:
+            fina_lst.append(this_str)
+            return
+
+        # while digits[0] is invalid(means relevant to no characters), remove it
+        while not conv_map[digits[0]]:
+            digits = digits[1:]
+
+        # recursive
+        for ch in conv_map[digits[0]]:
+            self.helper(digits[1:], this_str + ch, fina_lst)
 
 
