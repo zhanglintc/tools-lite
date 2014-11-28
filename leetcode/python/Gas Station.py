4e84b71408f199745cd181e871a9bec9139262d4
Lane <zhanglintc623@gmail.com>
2014-11-29 00:37:51 +0800

    INIT

---
+++ leetcode/python/Gas Station.py
@@ -0,0 +1,23 @@
+# Gas Station
+# for leetcode problems
+# 2014.11.29 by zhanglin
+
+# Problem:
+# There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
+
+# You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1).
+# You begin the journey with an empty tank at one of the gas stations.
+
+# Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
+
+# Note:
+# The solution is guaranteed to be unique.
+
+class Solution:
+    # @param gas, a list of integers
+    # @param cost, a list of integers
+    # @return an integer
+    def canCompleteCircuit(self, gas, cost):
+        pass
+
+
