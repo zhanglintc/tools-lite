d72c380d0a70b24481ea5b2d05c927491999b077
Lane <zhanglintc623@gmail.com>
2014-11-28 23:59:42 +0800

    MIDDLE

--- leetcode/python/Reverse Linked List II.py
+++ leetcode/python/Reverse Linked List II.py
@@ -30,18 +30,45 @@ class Solution:
     # @return a ListNode
     def reverseBetween(self, head, m, n):
         dummy_head = ListNode(0)
+        dummy_head.next = head
         before_reverse = dummy_head
 
+        if m == n:
+            return head
+
         idx = 1
-        while idx != m:
+        while idx < m:
             head = head.next
             before_reverse = before_reverse.next
             idx += 1
 
-        pre_in_reverse = head
-        while idx != n:
-            pass
+        p_prev = before_reverse
+        p_this = head
+        p_next = head.next
+
+        while idx <= n and p_next:
+            p_this.next = p_prev
+            p_prev = p_this
+            p_this = p_next
+            p_next = p_next.next
+            idx += 1
+
+        before_reverse.next.next = p_next
+        before_reverse.next = p_this
+        p_this.next = p_prev
 
         return dummy_head.next
 
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+head = ListNode(3)
+head.next = ListNode(5)
+
+s = Solution()
+head = s.reverseBetween(head, 1, 2)
+
+print head.next.next
 
