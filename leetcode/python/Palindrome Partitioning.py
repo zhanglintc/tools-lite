6296b7547831e2c523bba6e97557f8544ada21e9
Lane <zhanglintc623@gmail.com>
2014-11-30 23:06:04 +0800

    INIT

---
+++ leetcode/python/Palindrome Partitioning.py
@@ -0,0 +1,22 @@
+# Palindrome Partitioning
+# for leetcode problems
+# 2014.11.30 by zhanglin
+
+# Problem:
+# Given a string s, partition s such that every substring of the partition is a palindrome.
+# Return all possible palindrome partitioning of s.
+
+# For example, given s = "aab",
+# Return
+#   [
+#     ["aa","b"],
+#     ["a","a","b"]
+#   ]
+
+class Solution:
+    # @param s, a string
+    # @return a list of lists of string
+    def partition(self, s):
+        pass
+
+
