e3c2e8665923a82c6ee3b989375b155f3d32e7d2
Lane <zhanglintc623@gmail.com>
2014-12-04 16:15:27 +0800

    INIT

---
+++ leetcode/python/Edit Distance.py
@@ -0,0 +1,20 @@
+# Edit Distance
+# for leetcode problems
+# 2014.12.04 by zhanglin
+
+# Problem:
+# Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2.
+# (each operation is counted as 1 step.)
+
+# You have the following 3 operations permitted on a word:
+
+# a) Insert a character
+# b) Delete a character
+# c) Replace a character
+
+class Solution:
+    # @return an integer
+    def minDistance(self, word1, word2):
+        pass
+
+
