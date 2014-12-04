b2561329c06b1f6772a089478550cddbc857a532
Lane <zhanglintc623@gmail.com>
2014-12-04 16:01:25 +0800

    AC
    理解有点难

--- leetcode/python/Palindrome Partitioning II.py
+++ leetcode/python/Palindrome Partitioning II.py
@@ -10,37 +10,26 @@
 # For example, given s = "aab",
 # Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
 
+# Hard to understand...
+# Refer to http://blog.csdn.net/doc_sgl/article/details/13418125
+# Refer to http://www.cnblogs.com/zuoyuan/p/3758783.html
 class Solution:
     # @param s, a string
     # @return an integer
     def minCut(self, s):
-        return min(self.partition_helper(s, 0), self.partition_helper(s[::-1], 0))
+        # dp for "dynamic plan", vp for "valid palindrome"
+        dp = [0 for i in range(len(s) + 1)] # dp is one step longer than len(s), think about if whole string is a palindrome
+        vp = [[False for j in range(len(s))] for i in range(len(s))]
 
-    def partition_helper(self, s, cuts):
-        for i in range(len(s) + 1, 0, -1):
-            if self.isPartition(s[:i]) and i == len(s) + 1:
-                return cuts
+        for i in range(len(s) + 1): # dp is one step longer than len(s), think about if whole string is a palindrome
+            dp[i] = len(s) - i # least palindrome strings from position i to the end
 
-            elif self.isPartition(s[:i]):
-                return self.partition_helper(s[i:], cuts + 1)
+        for i in range(len(s), -1, -1):
+            for j in range(i, len(s)):
+                if s[i] == s[j] and (j - i <= 1 or vp[i + 1][j - 1]): # if pos i to pos j is palindrome
+                    vp[i][j] = True
+                    dp[i] = min(dp[i], dp[j + 1] + 1) # if i to j is palindrome, then dp[i] equals dp[j + 1] + 1, update dp[j]
 
-            else:
-                continue
+        return dp[0] - 1 # cuts is one less than palindromes
 
-    def isPartition(self, s):
-        left  = 0
-        right = len(s) - 1
-
-        while left < right:
-            if s[left] != s[right]:
-                return False
-
-            left  += 1
-            right -= 1
-
-        return True
-
-
-s=Solution()
-print s.minCut('aaabaa')
 
