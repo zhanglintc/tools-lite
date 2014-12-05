5d9493d65793b8f194e5052436ca36e5c1a2c54f
Lane <zhanglintc623@gmail.com>
2014-12-05 15:08:21 +0800

    INIT

---
+++ leetcode/python/Distinct Subsequences.py
@@ -0,0 +1,22 @@
+# Distinct Subsequences
+# for leetcode problems
+# 2014.12.05 by zhanglin
+
+# Problem:
+# Given a string S and a string T, count the number of distinct subsequences of T in S.
+
+# A subsequence of a string is a new string which is formed from the original string bydeleting some (can be none)
+# of the characters without disturbing the relative positions of the remaining characters.
+# (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+
+# Here is an example:
+# S = "rabbbit", T = "rabbit"
+
+# Return 3.
+
+class Solution:
+    # @return an integer
+    def numDistinct(self, S, T):
+        pass
+
+
