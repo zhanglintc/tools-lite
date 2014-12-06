7edcffe3f5e87bbccb4dd5544f5f40ec9fbda87b
Lane <zhanglintc623@gmail.com>
2014-12-06 16:47:46 +0800

    AC
    hard to understand

--- leetcode/python/Distinct Subsequences.py
+++ leetcode/python/Distinct Subsequences.py
@@ -14,9 +14,24 @@
 
 # Return 3.
 
+# Refer to: http://blog.csdn.net/abcbc/article/details/8978146
 class Solution:
     # @return an integer
     def numDistinct(self, S, T):
-        pass
+        # dp length is dp[S + 1][T + 1]
+        # thus there is offset between dp and S or T
+        # dp[s][t] while S[s - 1], T[t - 1]
+        dp = [[0 for t in range(len(T) + 1)] for s in range(len(S) + 1)]
+        
+        # initialize
+        for s in range(len(S) + 1):
+            dp[s][0] = 1
+
+        # dynamic planning
+        for s in range(1, len(S) + 1):
+            for t in range(1, len(T) + 1):
+                dp[s][t] = (dp[s - 1][t] + dp[s - 1][t - 1]) if (S[s - 1] == T[t - 1]) else (dp[s - 1][t])
+
+        return dp[-1][-1]
 
 
