b8ab58602bb75ca3cd66c303ef547589c5a94f27
Lane <zhanglintc623@gmail.com>
2014-12-08 17:20:28 +0800

    AC
    headache

--- leetcode/python/Reverse Nodes in k-Group.py
+++ leetcode/python/Reverse Nodes in k-Group.py
@@ -25,6 +25,64 @@ class Solution:
     # @param k, an integer
     # @return a ListNode
     def reverseKGroup(self, head, k):
-        pass
+        p = head
+        length = 0
 
+        while p:
+            length += 1
+            p = p.next
+
+        dummy_head = ListNode(0)
+        dummy_head.next = head
+        last = dummy_head # always point position n * k - 1
+
+        while length >= k:
+            headPrev = last
+            headNext = head.next
+
+            idx = 1
+            while idx < k:
+                head.next = headPrev
+                headPrev = head
+                head = headNext
+                headNext = headNext.next
+                idx += 1
+
+            head.next = headPrev
+            last.next.next = headNext
+            tmp = last.next
+            last.next = head
+
+            # reset
+            last = tmp
+            headPrev = last
+            head = tmp.next
+            if head:
+                headNext = head.next
+
+            length -= k
+
+        return dummy_head.next
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
+
+
+s = Solution()
+head = s.reverseKGroup(head, 5)
+
+print head.val
+print head.next.val
+print head.next.next.val
+print head.next.next.next.val
+print head.next.next.next.next.val
 
