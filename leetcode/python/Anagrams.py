5541a9f3887fd30aee72f2cbd9350b24ee68bee6
Lane <zhanglintc623@gmail.com>
2014-12-08 14:17:11 +0800

    INIT

---
+++ leetcode/python/Anagrams.py
@@ -0,0 +1,16 @@
+# Anagrams
+# for leetcode problems
+# 2014.12.08 by zhanglin
+
+# Problem:
+# Given an array of strings, return all groups of strings that are anagrams.
+
+# Note: All inputs will be in lower-case.
+
+class Solution:
+    # @param strs, a list of strings
+    # @return a list of strings
+    def anagrams(self, strs):
+        pass
+
+
