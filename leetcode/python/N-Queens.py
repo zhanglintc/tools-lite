974d402ae3eb373c0c6773b5833e94015cdb4f6e
Lane <zhanglintc623@gmail.com>
2014-12-08 17:26:29 +0800

    INIT

---
+++ leetcode/python/N-Queens.py
@@ -0,0 +1,34 @@
+# N-Queens
+# for leetcode problems
+# 2014.12.08 by zhanglin
+
+# Problem:
+# The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+
+# Picture see: https://oj.leetcode.com/problems/n-queens/
+
+# Given an integer n, return all distinct solutions to the n-queens puzzle.
+# Each solution contains a distinct board configuration of the n-queens' placement,
+# where 'Q' and '.' both indicate a queen and an empty space respectively.
+
+# For example,
+# There exist two distinct solutions to the 4-queens puzzle:
+
+# [
+#  [".Q..",  // Solution 1
+#   "...Q",
+#   "Q...",
+#   "..Q."],
+
+#  ["..Q.",  // Solution 2
+#   "Q...",
+#   "...Q",
+#   ".Q.."]
+# ]
+
+class Solution:
+    # @return a list of lists of string
+    def solveNQueens(self, n):
+        pass
+
+
