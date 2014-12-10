a645bb8e60314290057b400cedcc2fe59aeb6f2e
Lane <zhanglintc623@gmail.com>
2014-12-11 00:00:31 +0800

    AC
    BFS and comment

--- leetcode/python/Clone Graph.py
+++ leetcode/python/Clone Graph.py
@@ -32,6 +32,7 @@
 #         self.label = x
 #         self.neighbors = []
 
+# DFS solution
 class Solution:
     # @param node, a undirected graph node
     # @return a undirected graph node
@@ -42,14 +43,71 @@ class Solution:
         return self.dfs(node, {})
 
     def dfs(self, node, visited):
+        # visited means copied, only return the copy
         if node in visited:
             return visited[node]
 
+        # not visited before, make a copy, and finally return
         newNode = UndirectedGraphNode(node.label)
+
+        # visited[old node] = its copy
         visited[node] = newNode
 
+        # fill newNode's neighbor list
         for neighbor in node.neighbors:
             newNode.neighbors.append(self.dfs(neighbor, visited))
+
+        return newNode
+
+# BFS solution
+class Solution:
+    # @param node, a undirected graph node
+    # @return a undirected graph node
+    def cloneGraph(self, node):
+        if not node:
+            return node
+
+        # store node which has already visited
+        visited = {}
+
+        # store node whose neighbors need to be visited
+        # value would be the copy of the old node
+        queue = []
+
+        # make a copy of base node
+        newNode = UndirectedGraphNode(node.label)
+
+        # visited[old node]  = its copy
+        visited[node] = newNode
+
+        # nodes in this list whose neighbor list need to be traversed
+        queue = [node]
+
+        # traverse the queue
+        for this in queue:
+
+            # visit all neighbors of this
+            for neighbor in this.neighbors:
+
+                # not copied before, make a copy
+                if neighbor not in visited:
+                    # make a copy
+                    clone = UndirectedGraphNode(neighbor.label)
+
+                    # visited[old node]  = its copy
+                    visited[neighbor] = clone
+
+                    # fill new node's neighbor list
+                    visited[this].neighbors.append(clone)
+
+                    # traverse this neighbor later
+                    queue.append(neighbor)
+
+                # already has copy, add the copy to neighbor list
+                else:
+                    # visited[this] is this new node whose neighbor list need to be filled
+                    # visited[neighbor] is a node already has a copy
+                    visited[this].neighbors.append(visited[neighbor])
 
         return newNode
 
