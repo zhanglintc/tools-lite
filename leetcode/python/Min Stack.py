f9a592f6fa0f484340560feec0f29c3a4119ff62
Lane <zhanglintc623@gmail.com>
2014-12-11 14:39:53 +0800

    INIT

---
+++ leetcode/python/Min Stack.py
@@ -0,0 +1,34 @@
+# Min Stack
+# for leetcode problems
+# 2014.12.11 by zhanglin
+
+# Problem:
+# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+# push(x) -- Push element x onto stack.
+# pop() -- Removes the element on top of the stack.
+# top() -- Get the top element.
+# getMin() -- Retrieve the minimum element in the stack.
+
+class MinStack:
+    # @param x, an integer
+    # @return an integer
+    def push(self, x):
+        pass
+        
+
+    # @return nothing
+    def pop(self):
+        pass
+        
+
+    # @return an integer
+    def top(self):
+        pass
+        
+
+    # @return an integer
+    def getMin(self):
+        pass
+
+
