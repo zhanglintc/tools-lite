45fe6a4bd462a0032f7a94dae7d17dd23b554ba8
Lane <zhanglintc623@gmail.com>
2014-12-11 15:15:12 +0800

    INIT

---
+++ leetcode/python/First Missing Positive.py
@@ -0,0 +1,20 @@
+# First Missing Positive
+# for leetcode problems
+# 2014.12.11 by zhanglin
+
+# Problem:
+# Given an unsorted integer array, find the first missing positive integer.
+
+# For example,
+# Given [1,2,0] return 3,
+# and [3,4,-1,1] return 2.
+
+# Your algorithm should run in O(n) time and uses constant space.
+
+class Solution:
+    # @param A, a list of integers
+    # @return an integer
+    def firstMissingPositive(self, A):
+        pass
+
+
