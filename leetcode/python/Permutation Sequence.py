1f18818e1f3704aa430500476a9e0cef92b79ece
Lane <zhanglintc623@gmail.com>
2014-12-11 17:37:14 +0800

    MIDDLE
    这尼玛又是什么鬼?!!!@#!@#!@#!!!!!
    连输入1, 1都超时, 超时你MB啊, caocaocao

--- leetcode/python/Permutation Sequence.py
+++ leetcode/python/Permutation Sequence.py
@@ -21,6 +21,32 @@
 class Solution:
     # @return a string
     def getPermutation(self, n, k):
-        pass
+        sequence = ['1', '2', '3', '4', '5','6','7', '8', '9'][:n]
+
+        for i in range(k - 1):
+            sequence = self.nextPermutation(sequence)
+
+        return ''.join(sequence)
+
+    def nextPermutation(self, num):
+        for i in range(len(num) - 2, -1, -1):
+            # find a number smaller than successor, called partition
+            if num[i] < num[i + 1]:
+                for j in range(len(num) - 1, i, -1):
+                    # find first number greater than partition
+                    if num[i] < num[j]:
+                        num[i], num[j] = num[j], num[i] # swap
+                        re = num[i + 1:] # and reverse numbers behind partition
+                        re.reverse()
+                        return num[:i + 1] + re
+
+                    else: # Newton's third law. You gotta leave something behind! (behind if)
+                        pass
+
+            else: # Newton's third law. You gotta leave something behind! (behind if)
+                pass
+
+        num.reverse()
+        return num
 
 
