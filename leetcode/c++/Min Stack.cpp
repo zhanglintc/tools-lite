e599bbe35e4d18ae4cd3014d6ad937ba36031ff0
Lane <zhanglintc623@gmail.com>
2014-12-12 15:03:24 +0800

    INIT

---
+++ leetcode/c++/Min Stack.cpp
@@ -0,0 +1,32 @@
+// Min Stack
+// for leetcode problems
+// 2014.12.12 by zhanglin
+
+// Problem:
+// Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+// push(x) -- Push element x onto stack.
+// pop() -- Removes the element on top of the stack.
+// top() -- Get the top element.
+// getMin() -- Retrieve the minimum element in the stack.
+
+class MinStack {
+public:
+    void push(int x) {
+        
+    }
+
+    void pop() {
+        
+    }
+
+    int top() {
+        
+    }
+
+    int getMin() {
+        
+    }
+};
+
+
