8d6dbb35aece4fb26e81b84a48e5bd8290928d33
Lane <zhanglintc623@gmail.com>
2014-12-23 14:12:17 +0800

    AC
    comment modification

--- leetcode/python/Permutation Sequence.py
+++ leetcode/python/Permutation Sequence.py
@@ -22,17 +22,17 @@ class Solution:
     # @return a string
     def getPermutation(self, n, k):
         numberlist = ['1', '2', '3', '4', '5','6','7', '8', '9']
-        sequence = ''
+        sequence = '' # final result
 
-        k -= 1 # don't know why...
+        k -= 1 # one offset
         while n:
-            facto = self.factorial(n - 1) # (n - 1)th factorial
-            this = numberlist[k / facto] # number to be added to final sequence
+            facto = self.factorial(n - 1) # (n - 1)'s factorial
+            this = numberlist[k / facto] # get the number to be added
 
             sequence += this # add this number
-            numberlist.remove(this) # remove it
+            numberlist.remove(this) # remove this number
             k %= facto # remaining k
-            n -= 1 # n - 1 numbers remaining
+            n -= 1 # (n - 1) numbers remaining
 
         return sequence
 
