d59857efb9da4d288b5acf316579aca561aca813
Lane <zhanglintc623@gmail.com>
2014-12-25 12:59:12 +0800

    AC

--- leetcode/python/Largest Rectangle in Histogram.py
+++ leetcode/python/Largest Rectangle in Histogram.py
@@ -38,26 +38,21 @@ class Solution:
     # @param height, a list of integer
     # @return an integer
     def largestRectangleArea(self, height):
-        if not height:
-            return 0
+        stk = [] # store every start of ascending chain
+        area = 0 # result
 
-        stk = []
-        area = height[0]
-        height.append(0)
-
-        for i in range(len(height)):
-            if not stk or height[stk[-1]] <= height[i]:
+        i = 0
+        height.append(0) # append a 0 makes the code more elegant
+        while i < len(height):
+            if not stk or height[i] > height[stk[-1]]: # ascending chain
                 stk.append(i)
+                i += 1
 
-            while height[stk[-1]] > height[i]:
-                thisIdx = stk.pop()
-                stk.append(i)
-                thisArea = height[thisIdx] * (i if not stk else (i - thisIdx))
-                area = thisArea if thisArea > area else area
+            else: # descending chain
+                this = stk.pop()
+                width = i if not stk else i - stk[-1] - 1
+                area = max(area, width * height[this]) # upadate max area
 
         return area
-
-s = Solution()
-print s.largestRectangleArea([2,1,5,6,2,3])
 
 
