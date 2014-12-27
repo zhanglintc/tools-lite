345f01ece665d6415a1329133065e90826308911
Lane <zhanglintc623@gmail.com>
2014-12-27 17:06:40 +0800

    MIDDLE
    边界条件为何如此操蛋…

--- leetcode/python/Excel Sheet Column Title.py
+++ leetcode/python/Excel Sheet Column Title.py
@@ -21,6 +21,25 @@
 class Solution:
     # @return a string
     def convertToTitle(self, num):
-        pass
+        ConvStr = "ZABCDEFGHIJKLMNOPQRSTUVWXY"
+        ExcelStr = ""
+        NumList = []
+        isMultiple = True if num % 26 == 0 else False
+
+        i = 1
+        while 26 ** i < num:
+            NumList.append(26 ** i)
+            i += 1
+
+        NumList.reverse()
+
+        for i in NumList:
+            ExcelStr += ConvStr[num / i - (1 if isMultiple else 0)]
+            num %= i
+
+        return ExcelStr + ConvStr[num % 26]
+
+s = Solution()
+print s.convertToTitle(701)
 
 
