253aff3495bb6918fae2e1ae5ab5eedb37012d49
Lane <zhanglintc623@gmail.com>
2014-12-31 00:22:29 +0800

    AC
    神解答...

--- leetcode/python/Excel Sheet Column Title.py
+++ leetcode/python/Excel Sheet Column Title.py
@@ -22,24 +22,6 @@ class Solution:
     # @return a string
     def convertToTitle(self, num):
         ConvStr = "ZABCDEFGHIJKLMNOPQRSTUVWXY"
-        ExcelStr = ""
-        NumList = []
-        isMultiple = True if num % 26 == 0 else False
-
-        i = 1
-        while 26 ** i < num:
-            NumList.append(26 ** i)
-            i += 1
-
-        NumList.reverse()
-
-        for i in NumList:
-            ExcelStr += ConvStr[num / i - (1 if isMultiple else 0)]
-            num %= i
-
-        return ExcelStr + ConvStr[num % 26]
-
-s = Solution()
-print s.convertToTitle(701)
+        return "" if num == 0 else self.convertToTitle((num - 1) / 26) + ConvStr[num % 26]
 
 
