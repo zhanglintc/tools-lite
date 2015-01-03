0647d1ed020cbf881a305de900dc472cc11d21d1
Lane <zhanglintc623@gmail.com>
2015-01-03 22:24:17 +0800

    INIT

---
+++ leetcode/python/Factorial Trailing Zeroes.py
@@ -0,0 +1,18 @@
+# Factorial Trailing Zeroes
+# for leetcode problems
+# 2015.01.03 by zhanglin
+
+# Problem:
+# Given an integer n, return the number of trailing zeroes in n!.
+
+# Note: Your solution should be in logarithmic time complexity.
+
+# Credits:
+# Special thanks to @ts for adding this problem and creating all test cases.
+
+class Solution:
+    # @return an integer
+    def trailingZeroes(self, n):
+        pass
+
+
