efba2370648fd193ab53017881fa91bfd1e0de07
Lane <zhanglintc623@gmail.com>
2015-01-05 00:11:20 +0800

    INIT

---
+++ leetcode/python/Maximum Gap.py
@@ -0,0 +1,23 @@
+# Maximum Gap
+# for leetcode problems
+# 2015.01.04 by zhanglin
+
+# Problem:
+# Given an unsorted array, find the maximum difference between the successive elements in its sorted form.
+
+# Try to solve it in linear time/space.
+
+# Return 0 if the array contains less than 2 elements.
+
+# You may assume all elements in the array are non-negative integers and fit in the 32-bit signed integer range.
+
+# Credits:
+# Special thanks to @porker2008 for adding this problem and creating all test cases.
+
+class Solution:
+    # @param num, a list of integer
+    # @return an integer
+    def maximumGap(self, num):
+        pass
+
+
