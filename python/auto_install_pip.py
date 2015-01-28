1ee7169b3f014bf2190cf68a097c020622c95ce6
Lane <zhanglintc623@gmail.com>
2015-01-28 13:34:51 +0800

    optimize auto_install_pip.py

--- python/auto_install_pip.py
+++ python/auto_install_pip.py
@@ -1,36 +1,30 @@
 # coding=utf-8
 
-import os
+import os, sys
 import urllib
 
 # down load get-pip.py
-def report_hook(count, block_size, total_size):
-    print '%02d%%'%(100.0 * count * block_size/ total_size)
+def report_hook(count, blockSize, totalSize):
+    percent = int(count * blockSize * 100 / totalSize)
+    sys.stdout.write("\r%d%%" % percent + ' completed')
+    sys.stdout.flush()
 
 url = 'https://raw.github.com/pypa/pip/master/contrib/get-pip.py'
-print 'download begin, please waite...'
+print('download begin, please wait...')
 
 file_path, headers = urllib.urlretrieve(url, reporthook = report_hook)
 
 with open(file_path, "rb") as fr:
     data = fr.read()
 
-with open('./download.py','w') as fw:
+with open('./download.py','wb') as fw:
     fw.write(data)
 
-print 'download success, installing...'
+print('')
+print('download success, installing...')
 
-# install pip by calling get-pip.py
+# install pip by calling get-pip.py(i.e. download.py)
 import download
 download.main()
 
-print 'clearing up...'
-filepath=['./download.py','./download.pyc']
 
-try:
-    for path in filepath:
-        os.remove(path)
-except :
-    pass
-
-print "insall pip success, please ADD '\Python27\Scripts' to your enveronment path"
