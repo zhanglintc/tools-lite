41dd849fb56234779b8d0a3dcfb3bed7cae4e091
Lane <zhanglintc623@gmail.com>
2015-02-02 12:50:42 +0800

    MacPriceCheck.py

---
+++ python/MacPriceCheck.py
@@ -0,0 +1,60 @@
+# coding = utf-8
+
+import os, re
+import urllib
+import subprocess
+
+mailto = ""
+def sendmail(type):
+    if type == "bingo":
+        send_content = "11.1 inches MacBook Air available now\nPlease check official site as soon as possible"
+        send_command = 'echo "{0}" | mutt -s "Mac Price Report" {1}'.format(send_content, mailto)
+
+        sp = subprocess.Popen(["/bin/bash", "-i", "-c", send_command])
+        sp.communicate()
+
+    elif type == "normal":
+        send_content = "this is hourly report\n11.1 inches MacBook Air is NOT available right now\nwe'll continue monitoring"
+        send_command = 'echo "{0}" | mutt -s "Mac Price Report" {1}'.format(send_content, mailto)
+
+        sp = subprocess.Popen(["/bin/bash", "-i", "-c", send_command])
+        sp.communicate()
+
+    else:
+        pass
+
+count = '0'
+
+with open("MacCnt.txt", "ab") as fa:
+    pass
+
+with open("MacCnt.txt", "rb") as fr:
+    read = fr.read()
+    if not read:
+        count = '0'
+    elif read == "60" or read = "0":
+        sendmail("normal")
+    else:
+        count = str(int(read) + 1)
+
+with open("MacCnt.txt", "wb") as fw:
+    fw.write(count)
+
+recv = urllib.urlopen("http://store.apple.com/hk-zh/browse/home/specialdeals/mac")
+content = recv.read()
+
+p = re.compile("HK\$.*")
+matches = p.findall(content)
+prices = []
+for match in matches:
+    match = match.replace("HK$", "")
+    match = match.replace(",", "")
+    prices.append(int(match))
+
+for price in prices:
+    if 5000 < price < 6000:
+        sendmail("bingo")
+    else:
+        print("not target")
+
+
