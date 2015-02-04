c86fa54fdb9f2658c589536d1fca6a65bf440094
Lane <zhanglintc623@gmail.com>
2015-02-04 16:29:35 +0800

    AC

--- leetcode/java/Word Break.java
+++ leetcode/java/Word Break.java
@@ -13,7 +13,18 @@
 // Return true because "leetcode" can be segmented as "leet code".
 public class Solution {
     public boolean wordBreak(String s, Set<String> dict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
         
+        for(int i = 1; i < s.length() + 1; i++) {
+            for(int j = 0; j < i; j++) {
+                if(dp[j] && dict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                }
+            }
+        }
+
+        return dp[s.length()];
     }
 }
 
