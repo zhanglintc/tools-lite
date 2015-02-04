cdf519f19bc7b286ca0328246e05b6ce55e38861
Lane <zhanglintc623@gmail.com>
2015-02-04 14:31:08 +0800

    INIT

---
+++ leetcode/java/Word Break.java
@@ -0,0 +1,20 @@
+// Word Break
+// for leetcode problems
+// 2015.02.04 by zhanglin
+
+// Problem:
+// Given a string s and a dictionary of words dict,
+// determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+// For example, given
+// s = "leetcode",
+// dict = ["leet", "code"].
+
+// Return true because "leetcode" can be segmented as "leet code".
+public class Solution {
+    public boolean wordBreak(String s, Set<String> dict) {
+        
+    }
+}
+
+
