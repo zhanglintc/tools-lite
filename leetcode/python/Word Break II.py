f5ebeaf0891ffd0e982e11f437f1cb164b556d50
Lane <zhanglintc623@gmail.com>
2015-02-04 16:39:13 +0800

    INIT

---
+++ leetcode/python/Word Break II.py
@@ -0,0 +1,24 @@
+# Word Break II
+# for leetcode problems
+# 2015.02.04 by zhanglin
+
+# Problem:
+# Given a string s and a dictionary of words dict,
+# add spaces in s to construct a sentence where each word is a valid dictionary word.
+
+# Return all such possible sentences.
+
+# For example, given
+# s = "catsanddog",
+# dict = ["cat", "cats", "and", "sand", "dog"].
+
+# A solution is ["cats and dog", "cat sand dog"].
+
+class Solution:
+    # @param s, a string
+    # @param dict, a set of string
+    # @return a list of strings
+    def wordBreak(self, s, dict):
+        pass
+
+
