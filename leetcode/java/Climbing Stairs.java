0d22a554784e471bb73ba122a04d97c11afd93db
Lane <zhanglintc623@gmail.com>
2015-02-04 22:48:24 +0800

    AC

--- leetcode/java/Climbing Stairs.java
+++ leetcode/java/Climbing Stairs.java
@@ -9,7 +9,22 @@
 
 public class Solution {
     public int climbStairs(int n) {
-        ;
+        if(n == 1)
+            return 1;
+
+        if(n == 2)
+            return 2;
+
+        int[] dp = new int[n];
+
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for(int i = 2;i < n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+
+        return dp[n - 1];
     }
 }
 
