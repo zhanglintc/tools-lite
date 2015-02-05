bd00f69dd4013bd33b925a74c3b0d4f23a1f70f9
Lane <zhanglintc623@gmail.com>
2015-02-05 16:40:40 +0800

    AC

--- leetcode/python/Restore IP Addresses.py
+++ leetcode/python/Restore IP Addresses.py
@@ -14,6 +14,25 @@ class Solution:
     # @param s, a string
     # @return a list of strings
     def restoreIpAddresses(self, s):
-        pass
+        fina_lst = []
+        self.helper(s, 0, "", fina_lst)
+        return fina_lst
+
+    def helper(self, s, times, IpAdd, fina_lst):
+        if not s or times == 4:
+            if not s and times == 4:
+                fina_lst.append(IpAdd[1:])
+
+            return
+
+        for i in range(1, len(s) + 1):
+            if 0 <= int(s[:i]) <= 255 and self.isValidIp(s[:i]):
+                self.helper(s[i:], times + 1, IpAdd + "." + s[:i], fina_lst)
+
+    def isValidIp(self, ip):
+        if len(ip) > 1 and ip[0] == "0":
+            return False
+
+        return True
 
 
