a2d2d9ed89720bd37f302268e594c19e4031b364
Lane <zhanglintc623@gmail.com>
2015-02-05 13:23:22 +0800

    INIT

---
+++ leetcode/python/Restore IP Addresses.py
@@ -0,0 +1,19 @@
+# Restore IP Addresses
+# for leetcode problems
+# 2015.02.05 by zhanglin
+
+# Problem:
+# Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+
+# For example:
+# Given "25525511135",
+
+# return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
+
+class Solution:
+    # @param s, a string
+    # @return a list of strings
+    def restoreIpAddresses(self, s):
+        pass
+
+
