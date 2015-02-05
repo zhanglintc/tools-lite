6666dbe6325793fd4dd460f9a50237502ad5d34e
Lane <zhanglintc623@gmail.com>
2015-02-05 13:13:11 +0800

    AC

--- leetcode/python/Word Break II.py
+++ leetcode/python/Word Break II.py
@@ -19,6 +19,29 @@ class Solution:
     # @param dict, a set of string
     # @return a list of strings
     def wordBreak(self, s, dict):
-        pass
+        fina_lst = []
+        if self.hasAnswer(s, dict):
+            self.getAnswer(s, dict, "", fina_lst)
+
+        return fina_lst
+
+    def hasAnswer(self, s, dict):
+        dp = [False for i in range(len(s) + 1)]
+        dp[0] = True
+
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in dict:
+                    dp[i] = True
+
+        return dp[-1]
+
+    def getAnswer(self, s, dict, this_lst, fina_lst):
+        if not s:
+            fina_lst.append(this_lst[1:])
+
+        for i in range(1, len(s) + 1):
+            if s[:i] in dict:
+                self.getAnswer(s[i:], dict, this_lst + " " + s[:i], fina_lst)
 
 
