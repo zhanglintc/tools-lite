09863f75b49c8a20394a45582cae4384494cdb36
Lane <zhanglintc623@gmail.com>
2015-02-10 19:57:48 +0800

    INIT

---
+++ leetcode/python/Maximal Rectangle.py
@@ -0,0 +1,15 @@
+# Maximal Rectangle
+# for leetcode problems
+# 2015.02.10 by zhanglin
+
+# Problem:
+# Given a 2D binary matrix filled with 0's and 1's,
+# find the largest rectangle containing all ones and return its area.
+
+class Solution:
+    # @param matrix, a list of lists of 1 length string
+    # @return an integer
+    def maximalRectangle(self, matrix):
+        pass
+
+
