2b8dbd472f7bec4c09e24981dc7b631c73fca9ca
Lane <zhanglintc623@gmail.com>
2015-03-18 16:19:23 +0800

    AC
    好久没有更新了, 这道题居然是一个多月以前INIT的...

--- leetcode/python/Maximal Rectangle.py
+++ leetcode/python/Maximal Rectangle.py
@@ -6,10 +6,62 @@
 # Given a 2D binary matrix filled with 0's and 1's,
 # find the largest rectangle containing all ones and return its area.
 
+# see Largest Rectangle in Histogram.py
 class Solution:
     # @param matrix, a list of lists of 1 length string
     # @return an integer
+
+    # see Largest Rectangle in Histogram.py
+    def largestRectangleArea(self, height):
+        stk = [] # store every start of ascending chain
+        area = 0 # result
+
+        i = 0
+        height.append(0) # append a 0 makes the code more elegant
+        while i < len(height):
+            if not stk or height[i] >= height[stk[-1]]: # ascending chain
+                stk.append(i)
+                i += 1
+
+            else: # descending chain
+                this = stk.pop() # use this height
+                width = i if not stk else i - stk[-1] - 1 # but use the previous check point(don't forget 1 offset)
+                area = max(area, width * height[this]) # upadate max area
+
+        return area
+
+    def transformMatrix(self, matrix):
+        """
+        Input:                  Output:
+            matrix = [              matrix = [
+                "111",                  [1,1,1],
+                "111",                  [1,1,1],
+                "101"]                  [1,0,1]]
+        """
+        # make string to list, and make string to int
+        for row in range(len(matrix)):
+            matrix[row] = list(matrix[row])
+
+            for i in range(len(matrix[row])):
+                matrix[row][i] = int(matrix[row][i])
+
+        # transform matrix
+        for row in range(len(matrix)-1, -1, -1):
+            for line in range(len(matrix[row])):
+                if matrix[row][line]:
+                    fake_row = row - 1
+                    while matrix[fake_row][line] and fake_row >= 0:
+                        matrix[row][line] += 1
+                        fake_row -= 1
+
+    # main function
     def maximalRectangle(self, matrix):
-        pass
+        self.transformMatrix(matrix)
+
+        area = 0
+        for row in matrix:
+            area = max(area, self.largestRectangleArea(row))
+
+        return area
 
 
