4f626401b2e8abe1c62866810f1841e7e7cab5bb
Lane <zhanglintc623@gmail.com>
2015-03-20 18:40:53 +0800

    AC

--- leetcode/java/Same Tree.java
+++ leetcode/java/Same Tree.java
@@ -18,7 +18,15 @@
  */
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        
+        if(p == null && q == null) {
+            return true;
+        }
+
+        if((p == null && q != null) || (p != null && q == null) || (p.val != q.val)) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
 
