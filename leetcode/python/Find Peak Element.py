bcc240fe5d2f4029d4fe157bbb2c45115ef3061d
Lane <zhanglintc623@gmail.com>
2015-03-20 17:10:09 +0800

    AC

--- leetcode/python/Find Peak Element.py
+++ leetcode/python/Find Peak Element.py
@@ -23,7 +23,7 @@ class Solution:
         num = [None] + num + [None]
 
         for i in range(1, len(num) - 1):
-            if num[i - 1] < num[i] and num[i] > num[i + 1]:
+            if num[i - 1] < num[i] > num[i + 1]:
                 return i - 1
 
         return None
