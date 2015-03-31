5dd9783a7b6e341cb7f732f631153def2be684a0
Lane <zhanglintc623@gmail.com>
2015-03-31 14:47:41 +0800

    make code into function

--- misc/downPic/downPic.py
+++ misc/downPic/downPic.py
@@ -5,67 +5,76 @@ import os, urllib
 import datetime, time
 import re
 
+# global constants
+TARGET_URL = "http://stackoverflow.com/"
+
 CUR_TIME = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d_%H%M%S') # 20141110_151240
 LOG_FILE = CUR_TIME + '.log' # 20141110_151240.log
 
-TARGET_URL = "http://photo.163.com/foreverse@126/pp/slide/12703047.html#pid=38152323"
+# global variables
+siteName = re.search("http.?://(.*?)/.*", TARGET_URL + "/").group(1) # photo.163.com
+siteTitle = re.search("<title>(.*)</title>", urllib.urlopen(TARGET_URL).read()).group(1) # Stack Overflow
+folderName = "{}_{}".format(siteName, CUR_TIME) # photo.163.com_20141110_151240
 
-class AppURLopener(urllib.FancyURLopener):
-    version = "Mozilla/5.0"
- 
-urllib._urlopener = AppURLopener()
+def initial():
+    class AppURLopener(urllib.FancyURLopener):
+        version = "Mozilla/5.0"
+    urllib._urlopener = AppURLopener()
 
-mc = re.search("http.?://(.*?)/.*", TARGET_URL + "/")
-siteName = mc.group(1)
-folderName = "{}_{}".format(siteName, CUR_TIME)
+    if not os.path.exists(folderName):
+        os.system('mkdir "{}"'.format(folderName))
 
-if not os.path.exists(folderName):
-    os.system("mkdir {}".format(folderName))
+def downPic():
+    print("download start...\n")
 
-print("download start...\n")
+    web_cotent = urllib.urlopen(TARGET_URL) # open website
+    content = web_cotent.read()
+    mc = re.findall('src="(.*?jpg)"', content) # old pattern: http.//[^ ]*?jpg
 
-web_cotent = urllib.urlopen(TARGET_URL) # open website
-content = web_cotent.read()
-mc = re.findall('src="(.*?jpg)"', content) # old pattern: http.//[^ ]*?jpg
+    nextURL = True
+    while nextURL:
+        picNumbers = len(mc)
+        idx = 1
+        for url in mc:
+            if "http" not in url:
+                url = "http://" + siteName + url
 
-nextURL = True
-while nextURL:
-    picNumbers = len(mc)
-    idx = 1
-    for url in mc:
-        if "http" not in url:
-            url = "http://" + siteName + url
+            picName = url.split("/")[-1]
+            print("{} --- {}/{}".format(picName, idx, picNumbers))
+            idx += 1
 
-        picName = url.split("/")[-1]
-        print("{} --- {}/{}".format(picName, idx, picNumbers))
-        idx += 1
+            file_path, headers = urllib.urlretrieve(url)
+            with open(file_path, "rb") as fr:
+                data = fr.read()
 
-        file_path, headers = urllib.urlretrieve(url)
-        with open(file_path, "rb") as fr:
-            data = fr.read()
+            with open("{}/{}".format(folderName, picName), "wb") as fw:
+                fw.write(data)
 
-        with open("{}/{}".format(folderName, picName), "wb") as fw:
-            fw.write(data)
+        try:
+            nextURL = re.search('<a class="next" href="(.*?)">', content).group(1)
+            if "http" not in nextURL:
+                nextURL = "http://" + siteName + nextURL
+
+            web_cotent = urllib.urlopen(nextURL) # open website
+            content = web_cotent.read()
+            mc = re.findall('src="(.*?jpg)"', content) # old pattern: http.//[^ ]*?jpg
+
+            print("next page")
+
+        except:
+            nextURL = False
+
+    print("")
+    print("download completed...")
+
+if __name__ == "__main__":
+    initial()
+    downPic()
 
     try:
-        nextURL = re.search('<a class="next" href="(.*?)">', content).group(1)
-        if "http" not in nextURL:
-            nextURL = "http://" + siteName + nextURL
-
-        web_cotent = urllib.urlopen(nextURL) # open website
-        content = web_cotent.read()
-        mc = re.findall('src="(.*?jpg)"', content) # old pattern: http.//[^ ]*?jpg
-
-        print("next page")
-
+        input()
     except:
-        nextURL = False
+        pass
 
-print("")
-print("download completed...")
 
-try:
-    input()
-except:
-    pass
 
