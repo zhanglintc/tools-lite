6ff3f5058eee778e892d3304b97e2aa6a9e20857
Lane <zhanglintc623@gmail.com>
2015-05-10 16:03:36 +0800

    BingWallpapersCopy.cmd

---
+++ batch/BingWallpapersCopy.cmd
@@ -0,0 +1,3 @@
+@echo off
+xcopy /s /y C:\Users\%username%\AppData\Local\Microsoft\Windows\"Temporary Internet Files"\Enclosure\{BC63776D-EE80-4D5C-A7D4-9D5654B389DD} E:\Images\"Bing Wallpapers"
+::exit
