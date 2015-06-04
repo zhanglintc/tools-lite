28c577da43a4d0bf5977320c94737bc31992790c
lane <zhanglintc623@gmail.com>
2015-06-04 13:09:41 +0800

    make autopull.py versioned

---
+++ python/autopull.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+repos = [
+    #### zhanglintc ####
+    'git@github.com:zhanglintc/tools-lite.git',
+    'git@github.com:zhanglintc/leetcode.git',
+    'git@github.com:zhanglintc/wb.git',
+    'git@github.com:zhanglintc/snake.git',
+    'git@github.com:zhanglintc/SourcePrac.git',
+    'git@github.com:zhanglintc/zhanglintc.github.io.git',
+    'git@github.com:zhanglintc/5th.git',
+    'git@github.com:zhanglintc/sanguosha.git',
+    'git@github.com:zhanglintc/daDoudou.git',
+    'git@github.com:zhanglintc/SetWallpaper.git',
+
+    #### Theodolite ####
+    'git@github.com:Theodolite/3plus2.git',
+]
+
+# /home/lane
+base_path  = os.path.expanduser('~')
+dirs = os.listdir('{}/gitbak'.format(base_path))
+
+for repo in repos:
+    repoName = repo.split('/')[1][:-4]
+    if repoName not in dirs:
+        os.system('cd {}/gitbak && git clone {}'.format(base_path, repo))
+
+    else:
+        os.system('cd {}/gitbak/{} && git pull'.format(base_path, repoName))
+
+os.system('date >> {}/gitbak/gitbak.log'.format(base_path))
+# os.system('rm {}/sent'.format(base_path))
+
+
