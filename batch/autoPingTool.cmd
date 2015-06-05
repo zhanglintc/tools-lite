c18273ca0d141ec20ddef524f1ca0f44226454c3
Lane <zhanglintc623@gmail.com>
2015-06-05 15:04:43 +0800

    ZDS version

--- batch/autoPingTool.cmd
+++ batch/autoPingTool.cmd
@@ -17,12 +17,13 @@ echo ŽžF%time%>>IPlog.txt
 echo=>>IPlog.txt
 
 :loop
-ping -n 1 10.194.152.%IP%|findstr "out"&&(
-echo No%cnt%¨10.194.152.%IP%>>IPlog.txt
+echo Pinging: 192.168.39.%IP%
+ping -n 1 192.168.39.%IP%|findstr "‚Å‚«‚Ü‚¹‚ñ"&&(
+echo No%cnt%¨192.168.39.%IP%>>IPlog.txt
 set /a cnt=!cnt!+1
 )
 set /a IP=%IP%+1
-if %IP% LEQ 126 goto loop
+if %IP% LEQ 255 goto loop
 set /a cnt=%cnt%-1
 echo=>>IPlog.txt
 echo %cnt%‘äping‚Å‚«‚È‚¢>>IPlog.txt
