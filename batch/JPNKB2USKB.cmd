9646049e45eb71ca8ff67c45d03bb135eac567df
Lane <zhanglintc623@gmail.com>
2015-08-01 01:55:44 +0800

    display current setting & add choice

--- batch/JPNKB2USKB.cmd
+++ batch/JPNKB2USKB.cmd
@@ -2,10 +2,33 @@
 title Powered by Lane at ZDS
 color 3E
 
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\00000411" /v "Layout File" /t REG_SZ /d "KBDUS.DLL" /f > nul
+echo Current setting:
+echo ==================================
+reg query "HKLM\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\00000411"
+echo ==================================
+echo.
 
-echo Set 0411(Japanese) to KBDUS.DLL(US keyboard) successfull.
+:loop
+set op=null
+set /p op=Are you sure to change "Layout File" to "KBDUS.DLL"? [Y/N]
+if %op%==Y goto set
+if %op%==y goto set
+if %op%==N goto cancel
+if %op%==n goto cancel
+goto loop
+
+:set
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\00000411" /v "Layout File" /t REG_SZ /d "KBDUS.DLL" /f > nul
+echo.
+echo Set 0411(JPN) "Layout File" to "KBDUS.DLL"(US keyboard) successfull.
 echo Please restart your computer to make this change take effect.
+goto end
+
+:cancel
+echo.
+echo You haven't changed anything.
+
+:end
 echo.
 echo Press any to close...
 pause > nul
