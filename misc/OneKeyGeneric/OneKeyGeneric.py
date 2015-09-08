92f283dc424d4d71dce9c2699b5f744ed66376f4
Lane <zhanglintc623@gmail.com>
2015-09-08 18:41:44 +0800

    no prompt if no content replaced

--- misc/OneKeyGeneric/OneKeyGeneric.py
+++ misc/OneKeyGeneric/OneKeyGeneric.py
@@ -91,16 +91,13 @@ def ProcessFile(fPathName):
     for line in generator:
         # No.1 deal with *INF String
         for i in range(len(oldINFStr)):
-            if line.find(oldINFStr[i].strip()) != -1:
+            if line.find(oldINFStr[i].strip()) != -1 and oldINFStr[i].strip():
                 line = line.replace(oldINFStr[i].strip(), newINFStr[i].strip())
                 IsReplace = True
 
-        # No.2 KOAY**_*.***  ->  KOAY**A*.*** 
-        if re.search('(KOAY..)[^A](.)', line): # old pattern: (KOAYC.).(.)
-            if re.search('KOAY._COPY', line) or re.search('KOAY._DATA', line): # jump KOAY8_COPY or KOAY8_DATA
-                line = line
-            else:
-                line = re.sub('(KOAY..)[^A](.)', lambda mc: mc.group(1) + 'A' + mc.group(2), line)
+        # No.2 KOAY**_*.***  ->  KOAY**A*.***  @but not change _COPY or _DATA
+        if re.search('(KOAY..)[^A](.)', line) and not re.search('KOAY._COPY', line) and not re.search('KOAY._DATA', line):
+            line = re.sub('(KOAY..)[^A](.)', lambda mc: mc.group(1) + 'A' + mc.group(2), line)
             IsReplace = True
 
         # No.3 deal with KONICA MINOLTA
@@ -115,7 +112,7 @@ def ProcessFile(fPathName):
 
         # No.5 deal with C658
         for i in range(len(OwnName)):
-            if line.find(OwnName[i].strip()) != -1:
+            if line.find(OwnName[i].strip()) != -1 and OwnName[i].strip():
                 line = line.replace(OwnName[i].strip(), GenName[i].strip())
                 IsReplace = True
 
@@ -145,20 +142,27 @@ def ProcessFile(fPathName):
         print("replced file: {}".format(fPathName))
 
 if __name__ == '__main__':
+    # No.1: replace file content
+    print("Replace file content:")
     FTuple = os.walk(target_folder)
     for root,dirs,files in FTuple:
         for Tmpfile in files:
-            # No.1: replace file content
             if IsTargetFile(Tmpfile):
                 of = os.path.join(root,Tmpfile)
                 ProcessFile(of)
 
-            # No.2: replace file name
+    # No.2: replace file name
+    print("")
+    print("Replace file name:")
+    FTuple = os.walk(target_folder)
+    for root,dirs,files in FTuple:
+        for Tmpfile in files:
             if re.search('(KOAY..)[^A](.)', Tmpfile):
                 replaced_file = re.sub('(KOAY..)[^A](.)', lambda mc: mc.group(1) + 'A' + mc.group(2), Tmpfile)
                 old_file = os.path.join(root, Tmpfile)
                 new_file = os.path.join(root, replaced_file)
                 os.rename(old_file, new_file)
+                print("{0} => {1}".format(old_file.split("\\")[-1], new_file.split("\\")[-1]))
 
 
 
