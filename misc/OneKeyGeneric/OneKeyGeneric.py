b3563b8a11a3232c714d41ba29de948538f32c09
Lane <zhanglintc623@gmail.com>
2015-09-08 18:22:47 +0800

    make format pretty

--- misc/OneKeyGeneric/OneKeyGeneric.py
+++ misc/OneKeyGeneric/OneKeyGeneric.py
@@ -53,19 +53,6 @@ if len(OwnName) != len(GenName) or len(OwnName) != len(oldINFStr):
 # Need to be update -E
 ################################
 
-OrgFileName = """
-thi_is_a_string_that_no_content_can_match,
-""".split(",")
-for i in range(len(OrgFileName)):
-    OrgFileName[i] = OrgFileName[i].strip()    
-NewFileName = """
-whatever_strings_here,
-""".split(",")
-for i in range(len(NewFileName)):
-    NewFileName[i] = NewFileName[i].strip()
-for i in range(len(fExt)):
-    fExt[i] = fExt[i].strip().upper()
-
 ################################################################
 def IsTargetFile(FileName):
     sufix = os.path.splitext(FileName)[1][1:]
@@ -82,43 +69,32 @@ def ProcessFile(fPathName):
     """
     Replace content in files.
     """
-################################
-	# ingore files
-    file_name = fPathName.split('\\')[-1].lower()
+
+    file_name  = fPathName.split('\\')[-1].lower()
+    langFolder = fPathName.split("\\")[-2].upper()
+    TxtFile    = TxtFileHandle.TxtFileHandle()
+    generator  = TxtFile.ReadTxtFile(fPathName)
+
+    # ignore localize and cst
     if "localize" in file_name or file_name == "cst.ini":
         return
-################################
-    # get language folder
-    langFolder = fPathName.split("\\")[-2].upper()
-################################
-    TxtFile = TxtFileHandle.TxtFileHandle()
-    generator = TxtFile.ReadTxtFile(fPathName)
 
+    # fail to open file
     if generator == "":
         print("Error Read file:{}".format(fPathName))
         return
-################################
-    to_be_written = ""
-    IsReplace = False
-    jumpOneLine = False
-    for line in generator:
-        idx = 0
-################################################################
-        # No.0 replace by key word
-        for keyword in OrgFileName:
-            keywordstrip  = keyword.strip()
 
-            if line.find(keywordstrip) != -1:
-                line = line.replace(keywordstrip, NewFileName[idx])
-                IsReplace = True
-            idx = idx + 1
-################################################################
+    IsReplace   = False
+    jumpOneLine = False
+    to_be_written = ""
+
+    for line in generator:
         # No.1 deal with *INF String
         for i in range(len(oldINFStr)):
             if line.find(oldINFStr[i].strip()) != -1:
                 line = line.replace(oldINFStr[i].strip(), newINFStr[i].strip())
                 IsReplace = True
-################################################################
+
         # No.2 KOAY**_*.***  ->  KOAY**A*.*** 
         if re.search('(KOAY..)[^A](.)', line): # old pattern: (KOAYC.).(.)
             if re.search('KOAY._COPY', line) or re.search('KOAY._DATA', line): # jump KOAY8_COPY or KOAY8_DATA
@@ -126,23 +102,23 @@ def ProcessFile(fPathName):
             else:
                 line = re.sub('(KOAY..)[^A](.)', lambda mc: mc.group(1) + 'A' + mc.group(2), line)
             IsReplace = True
-################################################################
+
         # No.3 deal with KONICA MINOLTA
         if line.find("KONICA MINOLTA") != -1 and "INC" not in line:
             line = line.replace("KONICA MINOLTA", "Generic")
             IsReplace = True
-################################################################
+
         # No.4 KOPROFDL -> GNPROFDL
         if line.find("KOPROFDL") != -1:
             line = line.replace("KOPROFDL", "GNPROFDL")
             IsReplace = True
-################################################################
+
         # No.5 deal with C658
         for i in range(len(OwnName)):
             if line.find(OwnName[i].strip()) != -1:
                 line = line.replace(OwnName[i].strip(), GenName[i].strip())
                 IsReplace = True
-################################################################
+
         # No.6 deal with [OEM URLS]
         if "JA" in fPathName or langFolder not in langList:
             if jumpOneLine == True:
@@ -159,29 +135,30 @@ def ProcessFile(fPathName):
             if "http://konicaminolta.com/" in line:
                 line = ""
                 jumpOneLine = True
-################################################################
+
         # append this line to the end
         to_be_written += line
-################################################################
+
     # do replace
     if IsReplace:
         TxtFile.WriteTxtFile(to_be_written)
         print("replced file: {}".format(fPathName))
-################################################################
-FTuple = os.walk(target_folder)
-for root,dirs,files in FTuple:
-    for Tmpfile in files:
-        # replace file content
-        if IsTargetFile(Tmpfile):
-            of = os.path.join(root,Tmpfile)
-            ProcessFile(of)
-################################################################
-        # replace file name
-        if re.search('(KOAY..)[^A](.)', Tmpfile):
-            replaced_file = re.sub('(KOAY..)[^A](.)', lambda mc: mc.group(1) + 'A' + mc.group(2), Tmpfile)
-            old_file = os.path.join(root, Tmpfile)
-            new_file = os.path.join(root, replaced_file)
-            os.rename(old_file, new_file)
-################################################################
+
+if __name__ == '__main__':
+    FTuple = os.walk(target_folder)
+    for root,dirs,files in FTuple:
+        for Tmpfile in files:
+            # No.1: replace file content
+            if IsTargetFile(Tmpfile):
+                of = os.path.join(root,Tmpfile)
+                ProcessFile(of)
+
+            # No.2: replace file name
+            if re.search('(KOAY..)[^A](.)', Tmpfile):
+                replaced_file = re.sub('(KOAY..)[^A](.)', lambda mc: mc.group(1) + 'A' + mc.group(2), Tmpfile)
+                old_file = os.path.join(root, Tmpfile)
+                new_file = os.path.join(root, replaced_file)
+                os.rename(old_file, new_file)
+
 
 
