99a0a4f1f98c2d8f8c8f15344be94d5628d81406
Lane <zhanglintc623@gmail.com>
2015-11-22 22:42:01 +0800

    try to decode

--- tmp/web/hello.py
+++ tmp/web/hello.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from cgi import parse_qs, escape
+from WXBizMsgCrypt import WXBizMsgCrypt
+
+sToken          = "c8tcRUW1j"
+sEncodingAESKey = "e6msYFTXeev0zxFNQpNCzq91SfzcAKBBn3CGXAJgd90"
+sAppId          = "wx1c77202393c1c41d"
+
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    print environ['PATH_INFO']
-    return 'Hello, web!'
+
+    # print "PATH_INFO: ", environ['PATH_INFO']
+    # print "QUERY_STRING: ", environ['QUERY_STRING']
+    # print "REQUEST_METHOD: ", environ['REQUEST_METHOD']
+
+    d = parse_qs(environ['QUERY_STRING'])
+    print d
+
+    wxDecrypt = WXBizMsgCrypt(sToken, sEncodingAESKey, sAppId)
+    ret ,decryp_xml = wxDecrypt.DecryptMsg(d["echostr"], d["msg_signature"], d["timestamp"], d["nonce"])
+
+    print "No.1: ", ret
+    print "No.2: ", decryp_xml
+
+    return ret
