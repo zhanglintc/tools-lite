0f3260d2a7ca2c09b9b3522f7ecc5f7e98a22a58
Lane <zhanglintc623@gmail.com>
2015-11-26 17:57:29 +0800

    getRequestBody()

--- wx/enterprise.py
+++ wx/enterprise.py
@@ -16,8 +16,25 @@ sToken          = "c8tcRUW1j"
 sEncodingAESKey = "e6msYFTXeev0zxFNQpNCzq91SfzcAKBBn3CGXAJgd90"
 sAppId          = "wx1c77202393c1c41d"
 
+def getRequestBody():
+    # the environment variable CONTENT_LENGTH may be empty or missing
+    try:
+        request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+
+    except (ValueError):
+        request_body_size = 0
+
+    # When the method is POST the query string will be sent
+    # in the HTTP request body which is passed by the WSGI server
+    # in the file like wsgi.input environment variable.
+    request_body = environ['wsgi.input'].read(request_body_size)
+
+    return request_body
+
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
+
+    print getRequestBody()
 
     sReplyEchoStr = ""
     if "echostr" in environ['QUERY_STRING']:
