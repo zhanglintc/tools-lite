1d7d76484b0ab3acadfcfbdb4f7f6f1a7b81e4eb
Lane <zhanglintc623@gmail.com>
2015-11-27 17:34:28 +0800

    switch by event_key

--- wx/enterprise.py
+++ wx/enterprise.py
@@ -5,6 +5,8 @@ from cgi import parse_qs, escape
 from encrypt.WXBizMsgCrypt import WXBizMsgCrypt
 from tools.getCommit import getCommit
 
+import xml.etree.cElementTree as ET
+
 import urllib, urllib2
 import json
 import requests
@@ -46,28 +48,45 @@ def getRequestBody(environ):
 
     return request_body
 
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-
-    request_body = getRequestBody(environ)
-
+def verifyCallbackMode(environ):
     d = parse_qs(environ['QUERY_STRING'])
 
-    wxDecrypt = WXBizMsgCrypt(sToken, sEncodingAESKey, sAppId)
-    ret, xml_content = wxDecrypt.DecryptMsg(request_body, d["msg_signature"][0], d["timestamp"][0], d["nonce"][0])
+    wx = WXBizMsgCrypt(sToken, sEncodingAESKey, sAppId)
+    ret, sReplyEchoStr = wx.VerifyURL(d["msg_signature"][0], d["timestamp"][0], d["nonce"][0], d["echostr"][0])
 
-    sReplyEchoStr = ""
+    return sReplyEchoStr
+
+def application(environ, start_response):
+    # response content
+    start_response('200 OK', [('Content-Type', 'text/html')])
+
+    wx = WXBizMsgCrypt(sToken, sEncodingAESKey, sAppId)
+    d = parse_qs(environ['QUERY_STRING'])
+
+    # set up weixin callback mode
     if "echostr" in environ['QUERY_STRING']:
-        d = parse_qs(environ['QUERY_STRING'])
+        return verifyCallbackMode(environ)
 
-        wxDecrypt = WXBizMsgCrypt(sToken, sEncodingAESKey, sAppId)
-        ret ,sReplyEchoStr = wxDecrypt.VerifyURL(d["msg_signature"][0], d["timestamp"][0], d["nonce"][0], d["echostr"][0])
+    request_body = getRequestBody(environ)
+    ret, xml_content = wx.DecryptMsg(request_body, d["msg_signature"][0], d["timestamp"][0], d["nonce"][0])
+    xml_tree = ET.fromstring(xml_content)
 
-    # return sReplyEchoStr or "hello world"
-    ret, message = wxDecrypt.EncryptMsg(text_T.format(getCommit("https://github.com/zhanglintc?period=daily")), d["nonce"][0])
-    return message or "hello world"
+    touser_name   = xml_tree.find("ToUserName")
+    fromuser_name = xml_tree.find("FromUserName")
+    create_time   = xml_tree.find("CreateTime")
+    msg_type      = xml_tree.find("MsgType")
+    agent_ID      = xml_tree.find("AgentID")
+    event         = xml_tree.find("Event")
+    event_key     = xml_tree.find("EventKey")
 
-def sendSth():
+    if event_key == "V1001_GITHUB":
+        ret, message = wx.EncryptMsg(text_T.format(getCommit("https://github.com/zhanglintc?period=daily")), d["nonce"][0])
+        return message
+
+    # return a null string
+    return ""
+
+def createMenu():
     secret = "3AhT8A1akqYHKVuLCtrcx3OvZPFHbMO03vvBaGu4xyciG8Lj6z1OGs8Zp-81ZtnE"
     url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={0}&corpsecret={1}".format(sAppId, secret)
     access_token = ""
@@ -118,7 +137,7 @@ def sendSth():
     print requests.post("https://qyapi.weixin.qq.com/cgi-bin/menu/create?access_token={0}&agentid=0".format(access_token), data = params).text
 
 def main():
-    sendSth()
+    createMenu()
     # getCommit("https://github.com/zhanglintc?period=daily")
     pass
 
