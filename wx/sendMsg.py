e8603b4e7849b800a3c7b807b7a99f0b3f0a5dd4
Lane <zhanglintc623@gmail.com>
2015-11-27 22:57:08 +0800

    prompt

--- wx/sendMsg.py
+++ wx/sendMsg.py
@@ -45,14 +45,14 @@ def sendMsg(content = ""):
     params = json.dumps(params, ensure_ascii = False)
 
     resp = requests.post("https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={0}".format(access_token), data = params).text
-    print resp
+    print "from sendMsg: " + resp
     resp = json.loads(resp)
     if resp["errcode"] != 0:
-        print "Error {0}: {1}".format(resp["errcode"], resp["errmsg"])
+        print "from sendMsg: " + "Error {0}: {1}".format(resp["errcode"], resp["errmsg"])
 
         if resp["errcode"] == 42001 or resp["errcode"] == 40014:
             access_token = getToken()
-            print requests.post("https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={0}".format(access_token), data = params).text
+            print "from sendMsg: " + requests.post("https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={0}".format(access_token), data = params).text
 
 
 if __name__ == '__main__':
