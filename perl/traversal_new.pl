b2e41070bd00478e8f95e6518324bf2298eebf00
Lane <zhanglintc623@gmail.com>
2015-12-30 18:55:17 +0800

    update

--- perl/traversal_new.pl
+++ perl/traversal_new.pl
@@ -6,25 +6,36 @@ use File::Basename;
 # Refer to: http://www.chengxuyuans.com/Perl/62287.html
 # Refer to: http://www.cnblogs.com/itech/archive/2012/04/28/2468917.html
 sub each_file {
-    my $path = shift @_;    
+    my $path = shift @_;
+    my @lst = ();
+    &helper($path);
+    return @lst;
 
-    # 如果是文件夹，进入并遍历
-    if(-d $path) {
-        chdir $path or die "can't chdir $path:$!";
-        foreach (<*>) {
-            my $path = File::Spec->catfile( getcwd, $_ );
-            &each_file($path);
+    sub helper {
+        my $path = shift @_;
+
+        # 如果是文件夹，进入并遍历
+        if(-d $path) {
+            chdir $path or die "can't chdir $path:$!";
+            foreach (<*>) {
+                my $path = File::Spec->catfile( getcwd, $_ );
+                &each_file($path);
+            }
+
+            # 当前文件夹已经遍历完，回到上一级文件夹
+            my $dir_name = dirname $path;
+            chdir $dir_name or die "can't chdir $dir_name:$!";
         }
-
-        # 当前文件夹已经遍历完，回到上一级文件夹
-        my $dir_name = dirname $path;
-        chdir $dir_name or die "can't chdir $dir_name:$!";
-    }
-    else {
-        print "$path\n";
+        else {
+            # print "$path\n";
+            push @lst, $path;
+        }
     }
 }
 
-&each_file("E:/Git_Mine");
+
+foreach (&each_file("E:/Git_Mine")) {
+    print $_."\n";
+}
 
 
