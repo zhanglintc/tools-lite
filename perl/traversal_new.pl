4e9be8e9b699dd9fa862358e5565246c12a4d603
Lane <zhanglintc623@gmail.com>
2015-12-30 21:16:37 +0800

    update traversal_new.pl

--- perl/traversal_new.pl
+++ perl/traversal_new.pl
@@ -6,36 +6,32 @@ use File::Basename;
 # Refer to: http://www.chengxuyuans.com/Perl/62287.html
 # Refer to: http://www.cnblogs.com/itech/archive/2012/04/28/2468917.html
 sub each_file {
-    my $path = shift @_;
-    my @lst = ();
-    &helper($path);
-    return @lst;
+    my $givenFolder = shift @_;
 
-    sub helper {
+    our @pList; # path list to be returned
+    &innerEachFile($givenFolder);
+    return @pList;
+
+    sub innerEachFile {
         my $path = shift @_;
 
-        # 如果是文件夹，进入并遍历
         if(-d $path) {
-            chdir $path or die "can't chdir $path:$!";
+            chdir $path or die "can't chdir $path: $!\n";
             foreach (<*>) {
-                my $path = File::Spec->catfile( getcwd, $_ );
-                &each_file($path);
+                &innerEachFile(File::Spec->catfile(getcwd, $_));
             }
-
-            # 当前文件夹已经遍历完，回到上一级文件夹
             my $dir_name = dirname $path;
-            chdir $dir_name or die "can't chdir $dir_name:$!";
+            chdir $dir_name or die "can't chdir $dir_name: $!\n";
         }
         else {
-            # print "$path\n";
-            push @lst, $path;
+            push @pList, $path; # push file path into local @pList
         }
     }
 }
 
-
-foreach (&each_file("E:/Git_Mine")) {
-    print $_."\n";
+my $targetFolder = "/Users/lane/Github/leetcode";
+foreach (&each_file($targetFolder)) {
+    print "$_\n";
 }
 
 
