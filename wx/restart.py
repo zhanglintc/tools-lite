6753fc62a808d43934236c5b34d3d5f41eabc6b7
Lane <zhanglintc623@gmail.com>
2016-04-15 22:58:38 +0800

    add parameter

--- wx/restart.py
+++ wx/restart.py
@@ -1,7 +1,11 @@
-import os
+import os, sys
 
 print("killing makeServer.py")
 os.system("ps -ef | grep makeServer.py | grep -v grep | cut -c 9-15 | xargs kill -s 9")
 
+if len(sys.argv) == 2 and sys.argv[1] == "shut":
+    sys.exit(0)
+
 print("restarting makeServer.py")
 os.system("nohup python makeServer.py&")
+
