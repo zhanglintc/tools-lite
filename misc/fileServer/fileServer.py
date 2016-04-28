d8a1a5ecc4217427f6b14b32a06dc67015f7ecc6
Lane <zhanglintc623@gmail.com>
2016-04-28 16:55:20 +0800

    upload percent

--- misc/fileServer/fileServer.py
+++ misc/fileServer/fileServer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import SocketServer, os
+import SocketServer
+import struct
+import os
 
 # Format: name_len      --- one byte
 #         name          --- name_len bytes
@@ -15,13 +17,27 @@ class MyTCPHandler (SocketServer.StreamRequestHandler):
         name_len = ord(self.rfile.read(1))
         name = self.rfile.read(name_len)
         print "Get request: %s" % name
+        file_size = struct.unpack(">l", self.rfile.read(4))[0]
+        restfile = file_size
         fd = open(name, 'wb')
-        cont = self.rfile.read(4096)    
-        while cont:
-            fd.write(cont)
+        package_cnt = 0
+        while restfile > 4096:
+            package_cnt += 1
+
             cont = self.rfile.read(4096)
+            fd.write(cont)
+            restfile -= 4096
+
+            if package_cnt >= 5:
+                self.request.send(struct.pack('>l', file_size - restfile))
+                package_cnt = 0
+
+        self.request.send(struct.pack('>l', file_size - restfile))
+
+        fd.write(self.rfile.read(restfile))
         fd.close()
-        print "Out: %s" % name
+        print "Out: %s\n" % name
+
         os.system("sudo mv ./{0} /usr/local/tomcat/server/webapps/".format(name))
         print "Move: %s" % name
 
--- misc/fileServer/uploader.py
+++ misc/fileServer/uploader.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from socket import *
+import struct
 import os.path
 import sys
 
-target = ('115.29.192.240', 8988)
+target = ('localhost', 8988)
 
 def get_header(name):
     leng = len(name)
@@ -15,11 +16,22 @@ def get_header(name):
 def send_file(name):
     basename = os.path.basename(name)
     header = get_header(basename)
-    cont = open(name, "rb").read()
+    content = open(name, "rb").read()
+    file_size = len(content)
+    content = struct.pack(">l", file_size) + content
     s = socket(AF_INET, SOCK_STREAM)
     s.connect(target)
     s.sendall(header)
-    s.sendall(cont)
+    s.sendall(content)
+
+    float_file_size = float(file_size)
+    from_socket = s.recv(4)
+    while from_socket:
+        server_gotten = struct.unpack(">l", from_socket)[0]
+        sys.stdout.write("{0}%\r".format('%0.2f' % (server_gotten / float_file_size * 100)))
+        sys.stdout.flush()
+        from_socket = s.recv(4)
+
     s.close()
 
 while True:
@@ -27,5 +39,5 @@ while True:
     try:
         send_file(f)
         print 'Success: "%s" has been sent to server.\n' % os.path.basename(f)
-    except:
-        print "Error: File send has failed.\n"
+    except Exception, e:
+        print 'Error: %s\n' % e
