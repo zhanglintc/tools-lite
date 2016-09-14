cd2b9880b0fe8022ac8bf07fde9d155236633c88
Lane <zhanglintc623@gmail.com>
2016-09-14 18:03:18 +0800

    restart.rb

---
+++ ruby/restart.rb
@@ -0,0 +1,10 @@
+#!/env/bin/ruby
+# encoding: utf-8
+
+app_name = "bingAPI.rb"
+
+puts "killing #{app_name}"
+system "ps -ef | grep #{File.basename(app_name, ".*")} | grep -v grep | cut -c 9-15 | xargs kill -s 9"
+
+puts "restarting #{app_name}"
+system "nohup ruby #{app_name}&"
