b5d42eb0b6c508b9cf51dd89b8a92e54c44a79d4
zhanglintc <zhanglintc623@gmail.com>
2016-09-20 17:17:21 +0800

    bug fix

--- ruby/bingAPI.rb
+++ ruby/bingAPI.rb
@@ -5,26 +5,32 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 set :port, '5969'
 
-uri = URI("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN")
-raw = Net::HTTP.get(uri)
-jsn = JSON.parse raw
+def req_data
+  uri = URI("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN")
+  raw = Net::HTTP.get(uri)
+  jsn = JSON.parse raw
 
-enddate = jsn["images"][0]["enddate"]
-url = jsn["images"][0]["url"]
-copyright = jsn["images"][0]["copyright"]
+  $enddate = jsn["images"][0]["enddate"]
+  $url = jsn["images"][0]["url"]
+  $copyright = jsn["images"][0]["copyright"]
+end
 
 get '/' do
-  redirect url
+  req_data()
+  redirect $url
 end
 
 get '/url' do
-  redirect url
+  req_data()
+  redirect $url
 end
 
 get '/date' do
-  return enddate
+  req_data()
+  return $enddate
 end
 
 get '/description' do
-  return copyright
+  req_data()
+  return $copyright
 end
