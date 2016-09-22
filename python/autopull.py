3d1c1f231dfba507e785c11d2abff5b90e1f075b
zhanglintc <zhanglintc623@gmail.com>
2016-09-22 10:21:13 +0800

    gitbak()

--- python/autopull.py
+++ python/autopull.py
@@ -5,43 +5,51 @@ import urllib2
 import json
 import os
 
-repos = [
-    #### zhanglintc ####
-    'git@github.com:zhanglintc/tools-lite.git',
-    'git@github.com:zhanglintc/leetcode.git',
-    'git@github.com:zhanglintc/wb.git',
-    'git@github.com:zhanglintc/snake.git',
-    'git@github.com:zhanglintc/SourcePrac.git',
-    'git@github.com:zhanglintc/zhanglintc.github.io.git',
-    'git@github.com:zhanglintc/5th.git',
-    'git@github.com:zhanglintc/sanguosha.git',
-    'git@github.com:zhanglintc/daDoudou.git',
-    'git@github.com:zhanglintc/SetWallpaper.git',
+# repos = [
+#     #### zhanglintc ####
+#     'git@github.com:zhanglintc/tools-lite.git',
+#     'git@github.com:zhanglintc/leetcode.git',
+#     'git@github.com:zhanglintc/wb.git',
+#     'git@github.com:zhanglintc/snake.git',
+#     'git@github.com:zhanglintc/SourcePrac.git',
+#     'git@github.com:zhanglintc/zhanglintc.github.io.git',
+#     'git@github.com:zhanglintc/5th.git',
+#     'git@github.com:zhanglintc/sanguosha.git',
+#     'git@github.com:zhanglintc/daDoudou.git',
+#     'git@github.com:zhanglintc/SetWallpaper.git',
 
-    #### Theodolite ####
-    'git@github.com:Theodolite/3plus2.git',
-]
-
-repos = []
-recv = urllib2.urlopen("https://api.github.com/users/zhanglintc/repos")
-raw = recv.read()
-dikt = json.loads(raw)
-for item in dikt:
-    repos.append(item["ssh_url"])
+#     #### Theodolite ####
+#     'git@github.com:Theodolite/3plus2.git',
+# ]
 
 # /home/lane
-base_path  = os.path.expanduser('~')
-dirs = os.listdir('{}/gitbak'.format(base_path))
+base_path = os.path.expanduser('~')
 
-for repo in repos:
-    repoName = repo.split('/')[1][:-4]
-    if repoName not in dirs:
-        os.system('cd {}/gitbak && git clone {}'.format(base_path, repo))
+def gitbak(userName):
+    repos = []
+    recv = urllib2.urlopen("https://api.github.com/users/{0}/repos".format(userName))
+    raw = recv.read()
+    dikt = json.loads(raw)
+    for item in dikt:
+        repos.append(item["ssh_url"])
 
-    else:
-        os.system('cd {}/gitbak/{} && git pull'.format(base_path, repoName))
+    if not os.path.exists('{0}/gitbak/{1}'.format(base_path, userName)):
+        os.makedirs('{0}/gitbak/{1}'.format(base_path, userName))
 
-os.system('date >> {}/gitbak/gitbak.log'.format(base_path))
-# os.system('rm {}/sent'.format(base_path))
+    dirs = os.listdir('{0}/gitbak/{1}'.format(base_path, userName))
+
+    for repo in repos:
+        repoName = repo.split('/')[1][:-4]
+        if repoName not in dirs:
+            os.system('cd {0}/gitbak/{1} && git clone {2}'.format(base_path, userName, repo))
+
+        else:
+            os.system('cd {0}/gitbak/{1}/{2} && git pull'.format(base_path, userName, repo))
+
+if __name__ == '__main__':
+    gitbak("zhanglintc")
+    gitbak("Theodolite")
+
+os.system('date >> {0}/gitbak/gitbak.log'.format(base_path))
 
 
