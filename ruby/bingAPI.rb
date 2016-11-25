dfd56b2061c4f7778dcc07b516ad233568eaa8ab
Lane <zhanglintc623@gmail.com>
2016-11-25 21:30:49 +0800

    use daysAgo

--- ruby/bingAPI.rb
+++ ruby/bingAPI.rb
@@ -5,8 +5,8 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 set :port, '5969'
 
-def req_data
-  uri = URI("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN")
+def req_data daysAgo
+  uri = URI("http://www.bing.com/HPImageArchive.aspx?format=js&idx=#{daysAgo}&n=1&mkt=zh-CN")
   raw = Net::HTTP.get(uri)
   jsn = JSON.parse raw
 
@@ -16,21 +16,25 @@ def req_data
 end
 
 get '/' do
-  req_data()
+  daysAgo = (params['daysAgo'] or 0)
+  req_data(daysAgo)
   redirect $url
 end
 
 get '/url' do
-  req_data()
+  daysAgo = (params['daysAgo'] or 0)
+  req_data(daysAgo)
   redirect $url
 end
 
 get '/date' do
-  req_data()
+  daysAgo = (params['daysAgo'] or 0)
+  req_data(daysAgo)
   return $enddate
 end
 
 get '/description' do
-  req_data()
+  daysAgo = (params['daysAgo'] or 0)
+  req_data(daysAgo)
   return $copyright
 end
