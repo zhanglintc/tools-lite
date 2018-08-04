5e3ab609d9e2460ada59d8386e850dc1f85f3bff
zhanglintc <zhanglintc623@gmail.com>
2018-08-04 21:54:07 +0800

    update auto-renew.rb

--- ruby/auto-renew.rb
+++ ruby/auto-renew.rb
@@ -1,25 +1,43 @@
 # This is the script try to renew the ssl certificate
 # Written by zhanglintc at 2017.07.13
 
-# issue the certificate in folder "zhanglintc.co"
-# puts "issue the certificate"
-# `acme.sh  --issue --dns -d zhanglintc.co -d www.zhanglintc.co -d mmrz.zhanglintc.co -d zncx.zhanglintc.co`
-# puts "issue: done\n\n"
+# issue the certificate in folder "mmrz.zhanglintc.co"
+# `/home/lane/.acme.sh/acme.sh --issue --dns dns_dp -d mmrz.zhanglintc.co`
+# puts "issue the certificate => not enabled"
+
+domains = [
+    "mmrz.zhanglintc.co",
+    "navi.zhanglintc.co",
+    "zncx.zhanglintc.co",
+    "sxcx.zhanglintc.co",
+    "bing.zhanglintc.co",
+]
+
+domains = domains.map{ |e| e="-d #{e}"}
+domain_str = domains.join(" ")
+issue_str = "/home/lane/.acme.sh/acme.sh --issue --dns dns_dp #{domain_str}"
+
+puts "1. issue the certificate\n"
+puts "copy and execute the command below in other terminal, then press [ENTER]:\n\n"
+puts issue_str + "\n\n"
+
+puts "waiting for [ENTER]..."
+gets
+
+puts "issue: done\n\n"
 
 # renew the certificatea
-puts "renew the certificate"
-puts `/home/lane/.acme.sh/acme.sh --renew --force -d zhanglintc.co -d www.zhanglintc.co -d mmrz.zhanglintc.co -d zncw.zhanglintc.co`
-puts "renew: done\n\n"
+# puts "renew the certificate => not enabled"
+# `/home/lane/.acme.sh/acme.sh --renew -d mmrz.zhanglintc.co`
+# puts "renew: done\n\n"
 
 # copy the certificate to specific folder
-puts "copy the certificate"
-puts `sudo cp -r /home/lane/.acme.sh/zhanglintc.co /home/lane/ssl`
+puts "2. copy the certificate"
+`sudo cp -r /home/lane/.acme.sh/mmrz.zhanglintc.co /home/lane/ssl`
 puts "copy: done\n\n"
 
 # force reload configuration of Nginx
-puts "reload Nginx"
-puts `sudo service nginx force-reload`
+puts "3. reload Nginx"
+`sudo service nginx force-reload`
 puts "reload: done\n\n"
-
-
 
