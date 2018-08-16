b39fa3aab2f847320b8bd30499597edfd929afbf
zhanglintc <zhanglintc623@gmail.com>
2018-08-16 14:21:45 +0800

    format

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -1,42 +1,43 @@
 class RunnableDemo implements Runnable {
-   private Thread t;
-   private String threadName;
+    private Thread t;
+    private String threadName;
 
-   RunnableDemo( String name) {
-      threadName = name;
-      System.out.println("Creating " +  threadName );
-   }
+    RunnableDemo( String name) {
+        threadName = name;
+        // System.out.println("Creating " +  threadName );
+    }
 
    public void run() {
-      // System.out.println("Running " +  threadName );
-      try {
-      //    for(int i = 4; i > 0; i--) {
-      //       System.out.println("Thread: " + threadName + ", " + i);
-      //       // 让线程睡眠一会
-            Thread.sleep(15000);
-         // }
-      }catch (InterruptedException e) {
-         System.out.println("Thread " +  threadName + " interrupted.");
-      }
-      // System.out.println("Thread " +  threadName + " exiting.");
-      // for(;;);
-   }
+    try {
+        Thread.sleep(13000);
+    }catch (InterruptedException e) {
+        System.out.println("Thread " +  threadName + " interrupted.");
+    }
+        // for(;;);
+    }
 
-   public void start () {
-      // System.out.println("Starting " +  threadName );
-      // if (t == null) {
-         t = new Thread (this, threadName);
-         t.start ();
-      // }
-   }
+    public void start () {
+        t = new Thread (this, threadName);
+        t.start ();
+    }
 }
 
 public class HelloWorld {
-
     public static void main(String args[]) {
-        for (int i = 0; i < 100; ++i) {
-            new RunnableDemo( "Thread-1").start();
+        System.out.println("before 3s");
+        try {
+            Thread.sleep(3000);
+        }catch (InterruptedException e) {
+            System.out.println("Sleep failed");
         }
+        System.out.println("after 3s");
+
+        for (int i = 0; i < 100; ++i) {
+            new RunnableDemo("").start();
+        }
+
+        System.out.println("after thread start");
+
         while (true);
     }
 }
