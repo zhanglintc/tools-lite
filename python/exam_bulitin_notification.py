39dcaa71d341700a4129e8079918a10fc18aa07a
zhanglintc <zhanglintc623@gmail.com>
2019-09-18 11:12:11 +0800

    exam_bulitin_notification.py

---
+++ python/exam_bulitin_notification.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import json
+import queue
+import requests
+
+from concurrent import futures
+from bs4 import BeautifulSoup
+
+
+cpta_url = "http://www.cpta.com.cn/"
+json_db = "tmp/bulitin_json_db"
+
+
+def read_one_link(link):
+    res = requests.get(link)
+    res.encoding = "GB2312"
+    html = res.text
+
+    soup = BeautifulSoup(html, "html.parser")
+    title = soup.select_one('.text_con > #p_title').text
+    return title
+
+
+def main():
+    res = requests.get(cpta_url)
+    res.encoding = "GB2312"
+    html = res.text
+
+    soup = BeautifulSoup(html, "html.parser")
+    bulitin_li = soup.select('.list_14.clearfix > li')
+    bulitin_para_links = map(lambda x: x.a.get("href"), bulitin_li)
+    bulitin_para_links = map(lambda x: requests.compat.urljoin(cpta_url, x), bulitin_para_links)
+    bulitin_para_links = list(bulitin_para_links)
+
+    workers = min(len(bulitin_para_links), 20)
+    with futures.ThreadPoolExecutor(workers) as executor:
+        titles = executor.map(read_one_link, bulitin_para_links)
+
+    bulitin_history = []
+    if os.path.isfile(json_db):
+        with open(json_db, "r") as fr:
+            bulitin_history = fr.read()
+            try:
+                bulitin_history = json.loads(bulitin_history)
+            except ValueError:
+                pass
+
+    for title in titles:
+        if title not in bulitin_history:
+            bulitin_history.append(title)
+            print(title)
+
+    with open(json_db, "w") as fw:
+        fw.write(json.dumps(bulitin_history))
+
+
+if __name__ == '__main__':
+    main()
