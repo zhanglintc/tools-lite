4a8df88ecc18a2d9b2e96ed7528782f5e192f4af
zhanglintc <zhanglintc623@gmail.com>
2020-05-04 20:54:23 +0800

    Hashie.Mash

--- ruby/bingAPI.rb
+++ ruby/bingAPI.rb
@@ -1,49 +1,61 @@
+#!/usr/bin/env ruby
+
 require 'net/http'
 require 'json'
 require 'sinatra'
 require 'sinatra/jsonp'
+require 'hashie'
 
 set :bind, '0.0.0.0'
 set :port, '5969'
 
-def req_data daysAgo
+def parse_data daysAgo
   uri = URI("http://www.bing.com/HPImageArchive.aspx?format=js&idx=#{daysAgo}&n=1&mkt=zh-CN")
   raw = Net::HTTP.get(uri)
   jsn = JSON.parse raw
+  jsn = Hashie::Mash.new jsn
 
-  $enddate = jsn["images"][0]["enddate"]
-  $url = "http://www.bing.com" + jsn["images"][0]["url"]
-  $copyright = jsn["images"][0]["copyright"]
+  this = jsn.images[0]
+  $fullstartdate = this.fullstartdate
+  $startdate = this.startdate
+  $enddate = this.enddate
+  $url = "http://www.bing.com#{this.url}"
+  $urlbase = this.urlbase
+  $name = $urlbase.gsub "/th?id=", ""
+  $copyright = this.copyright
+  $title = this.copyright
+  $hash = this.hsh
+  p $name
 end
 
 get '/' do
   daysAgo = (params['daysAgo'] or 0)
-  req_data(daysAgo)
+  parse_data(daysAgo)
   redirect $url
 end
 
 get '/url' do
   daysAgo = (params['daysAgo'] or 0)
-  req_data(daysAgo)
+  parse_data(daysAgo)
   redirect $url
 end
 
 get '/date' do
   daysAgo = (params['daysAgo'] or 0)
-  req_data(daysAgo)
+  parse_data(daysAgo)
   return $enddate
 end
 
 get '/description' do
   daysAgo = (params['daysAgo'] or 0)
-  req_data(daysAgo)
+  parse_data(daysAgo)
   return $copyright
 end
 
 # get '/description_with_callback' do
 #   callback = (params['callback'] or 'default_callback')
 #   daysAgo = (params['daysAgo'] or 0)
-#   req_data(daysAgo)
+#   parse_data(daysAgo)
 #   jsn = {"description": $copyright}
 #   jsonp $copyright, callback
 # end
@@ -51,9 +63,18 @@ end
 get '/description_with_callback' do
   callback = (params['callback'] or 'default_callback')
   daysAgo = (params['daysAgo'] or 0)
-  req_data(daysAgo)
+  parse_data(daysAgo)
   response.headers['content-type'] = 'application/javascript; charset=utf-8'
-  ret = "#{callback}(\"#{$copyright}\", #{daysAgo})"
+  ret = "#{callback}(#{daysAgo}, \"#{$copyright}\")"
+end
+
+get '/check_fav_callback' do
+  callback = (params['callback'] or 'default_callback')
+  daysAgo = (params['daysAgo'] or 0)
+  parse_data(daysAgo)
+  in_db = 1
+  response.headers['content-type'] = 'application/javascript; charset=utf-8'
+  ret = "#{callback}(#{daysAgo}, #{in_db})"
 end
 
 
