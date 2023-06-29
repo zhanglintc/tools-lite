2f1b9d722625dd9331e33607074be66e0d2f960d
zhanglintc <zhanglintc623@gmail.com>
2023-06-29 15:22:16 +0800

    C7.cmd

---
+++ batch/C7.cmd
@@ -0,0 +1,4 @@
+@echo off
+rem "C:\Program Files\Oracle\VirtualBox\VBoxHeadless.exe" --startvm "C7"
+rem "C:\Program Files\Oracle\VirtualBox\VirtualBoxVM.exe" --startvm "C7" --type=headless
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm C7 --type=headless
