9c06c20f2781b30aa8d625bb5f32ea3a0c35c169
zhanglintc <zhanglintc623@gmail.com>
2026-01-29 10:34:08 +0800

    HTTP_X_REAL_IP

--- ruby/returnAddr.rb
+++ ruby/returnAddr.rb
@@ -7,6 +7,9 @@ set :bind, '0.0.0.0'
 set :port, '3322'
 
 get '/' do
-  return env['REMOTE_ADDR']
+  content_type :json
+  return request.env['HTTP_X_FORWARDED_FOR']&.split(',')&.first&.strip ||
+    request.env['HTTP_X_REAL_IP'] ||
+    request.env['REMOTE_ADDR']
 end
 
